3f91df28c941ce0d32c350b3f35ef790694205c8
Miralem Drek <mek@qlik.com>
2019-02-26 18:19:40 +0100

    feat(cli): add build command (#10)

--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 const yargs = require('yargs');
 
+const build = require('@nebula.js/cli-build/command');
 const create = require('@nebula.js/cli-create/command');
 const serve = require('@nebula.js/cli-serve/command');
 
 yargs.usage('nucleus <command> [options]')
+  .command(build)
   .command(create)
   .command(serve)
   .demandCommand()
