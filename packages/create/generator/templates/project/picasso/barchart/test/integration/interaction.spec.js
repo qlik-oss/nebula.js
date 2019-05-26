dbec30935dc6bdfae761273c59170ffac70b85dd
Miralem Drek <mm85dk@gmail.com>
2019-05-26 17:09:55 +0200

    test(cli-create): ci test generated barchart

--- packages/create/generator/templates/project/picasso/barchart/test/integration/interaction.spec.js
+++ packages/create/generator/templates/project/picasso/barchart/test/integration/interaction.spec.js
@@ -1,11 +1,10 @@
 describe('interaction', () => {
   const content = '.nebulajs-sn';
   it('should select two bars', async () => {
-    const app = encodeURIComponent('/apps/ctrl00.qvf');
+    const app = encodeURIComponent(process.env.APP_ID || '/apps/ctrl00.qvf');
     await page.goto(`${process.testServer.url}/render/app/${app}?cols=Alpha,=5+avg(Expression1)&&permissions=interact,select`);
-    await page.waitForSelector(content, {
-      timeout: 5000,
-    });
+    await page.waitForFunction(`!!document.querySelector('${content}')`);
+
     await page.click('rect[data-label="K"]');
     await page.click('rect[data-label="S"]');
     await page.click('button[title="Confirm"]');
