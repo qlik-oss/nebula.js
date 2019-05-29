bcbddadfd06263e7f653cd1289ef6df772814d14
Miralem Drek <mek@qlik.com>
2019-05-29 14:31:50 +0200

    test(cli-create): generate integration test for basic supernova

--- packages/create/generator/templates/project/none/test/integration/hello.spec.js
+++ packages/create/generator/templates/project/none/test/integration/hello.spec.js
@@ -2,7 +2,7 @@ describe('sn', () => {
   const content = '.nebulajs-sn';
   it('should say hello', async () => {
     const app = encodeURIComponent(process.env.APP_ID || '/apps/ctrl00.qvf');
-    await page.goto(`http://localhost:8085/render/app/${app}`);
+    await page.goto(`${process.testServer.url}/render/app/${app}`);
     await page.waitForFunction(`!!document.querySelector('${content}')`);
     const text = await page.$eval(content, el => el.textContent);
     expect(text).to.equal('Hello!');
