07277b228cd3442217c12c7f6e5837d4cc6fba73
Miralem Drek <mek@qlik.com>
2019-06-10 09:33:32 +0200

    docs(examples): update mashup to load resources from unpkg

--- examples/mashup/connect.js
+++ examples/mashup/connect.js
@@ -3,17 +3,17 @@ window.connect = function connect() {
 
   const createConnection = () => loadSchema().then(schema => window.enigma.create({
     schema,
-    url: `ws://${window.location.hostname || 'localhost'}:19076/app/${Date.now()}`,
+    url: `ws://${window.location.hostname || 'localhost'}:9076/app/${Date.now()}`,
   }).open().then(qix => qix.createSessionApp()));
 
   return createConnection().then(app => app.setScript(`
 Characters:
 Load Chr(RecNo()+Ord('A')-1) as Alpha, RecNo() as Num autogenerate 26;
-  
+
 ASCII:
-Load 
+Load
   if(RecNo()>=65 and RecNo()<=90,RecNo()-64) as Num,
-  Chr(RecNo()) as AsciiAlpha, 
+  Chr(RecNo()) as AsciiAlpha,
   RecNo() as AsciiNum
 autogenerate 85
   Where (RecNo()>=65 and RecNo()<=126) or RecNo()>=160;
