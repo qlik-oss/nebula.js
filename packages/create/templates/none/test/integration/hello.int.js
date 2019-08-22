9df777ad938c95d6aa25ca7b2ca3d3d4970e3169
Christoffer Åström <cam@qlik.com>
2019-08-22 15:54:44 +0200

    chore: use aw 6

---
+++ packages/create/templates/none/test/integration/hello.int.js
@@ -0,0 +1,10 @@
+describe('sn', () => {
+  const content = '.nebulajs-sn';
+  it('should say hello', async () => {
+    const app = encodeURIComponent(process.env.APP_ID || '/apps/ctrl00.qvf');
+    await page.goto(`${process.testServer.url}/render/app/${app}`);
+    await page.waitForFunction(`!!document.querySelector('${content}')`);
+    const text = await page.$eval(content, el => el.textContent);
+    expect(text).to.equal('Hello!');
+  });
+});
