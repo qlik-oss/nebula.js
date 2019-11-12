bcf5f685408c1e35e0d36b90b11b9c2f52937704
Christoffer Åström <stoffeastrom@gmail.com>
2019-11-12 09:24:43 +0100

    feat: add cell error screen (#170)

--- apis/nucleus/src/components/Cell.jsx
+++ apis/nucleus/src/components/Cell.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Paper, makeStyles } from '@material-ui/core';
 import { useTheme } from '@nebula.js/ui/theme';
 
 import Requirements from './Requirements';
@@ -9,6 +9,27 @@ import Header from './Header';
 import Footer from './Footer';
 import Supernova from './Supernova';
 import Placeholder from './Placeholder';
+
+const useStyles = makeStyles(() => ({
+  content: {
+    position: 'relative',
+    height: '100%',
+  },
+  contentError: {
+    '&::before': {
+      position: 'absolute',
+      height: '100%',
+      width: '100%',
+      top: 0,
+      left: 0,
+      content: '""',
+      backgroundSize: '14.14px 14.14px',
+      backgroundImage:
+        'linear-gradient(135deg, currentColor 10%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 50%, currentColor 50%, currentColor 59%, rgba(0,0,0,0) 60%, rgba(0,0,0,0) 103%)',
+      opacity: 0.1,
+    },
+  },
+}));
 
 const showRequirements = (sn, layout) => {
   if (!sn || !sn.generator || !sn.generator.qae || !layout || !layout.qHyperCube) {
@@ -23,25 +44,30 @@ const showRequirements = (sn, layout) => {
   return layout.qHyperCube.qDimensionInfo.length < minD || layout.qHyperCube.qMeasureInfo.length < minM;
 };
 
-const Content = ({ children }) => (
-  <div style={{ position: 'relative', height: '100%' }}>
-    <div
-      className="nebulajs-sn"
-      style={{
-        position: 'absolute',
-        top: '8px',
-        left: '8px',
-        right: '8px',
-        bottom: '8px',
-      }}
-    >
-      {children}
+const Content = React.forwardRef(({ children, showError }, ref) => {
+  const { content, contentError } = useStyles();
+  const classes = [content, showError ? contentError : ''];
+  return (
+    <div ref={ref} className={classes.join(' ')}>
+      <div
+        className="nebulajs-sn"
+        style={{
+          position: 'absolute',
+          top: '8px',
+          left: '8px',
+          right: '8px',
+          bottom: '8px',
+        }}
+      >
+        {children}
+      </div>
     </div>
-  </div>
-);
+  );
+});
 
 export default function Cell({ api, onInitial }) {
   const [, setChanged] = useState(0);
+
   const theme = useTheme();
   useEffect(() => {
     const onChanged = () => setChanged(Date.now());
@@ -60,7 +86,8 @@ export default function Cell({ api, onInitial }) {
 
   const SN = showRequirements(objectProps.sn, objectProps.layout) ? Requirements : Supernova;
   const Comp = !objectProps.sn ? Placeholder : SN;
-  const err = objectProps.error || false;
+  const showError = objectProps.error || objectProps.dataErrors.length;
+
   return (
     <Paper style={{ height: '100%', position: 'relative' }} elevation={0} square className="nebulajs-cell">
       <Grid container direction="column" spacing={0} style={{ height: '100%', padding: theme.spacing(1) }}>
@@ -70,9 +97,9 @@ export default function Cell({ api, onInitial }) {
           </Header>
         </Grid>
         <Grid item xs>
-          <Content>
-            {err ? (
-              <CError message={err.message} />
+          <Content showError={showError}>
+            {showError ? (
+              <CError err={objectProps.err} dataErrors={objectProps.dataErrors} />
             ) : (
               <Comp
                 key={objectProps.layout.visualization}
