a2a38bbf913c6f37e187f2b895431d0c0853813d
Miralem Drek <mek@qlik.com>
2019-11-19 11:16:52 +0100

    chore: setup codesandbox ci

---
+++ .codesandbox/mekko/src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+import init from "./init";
+
+init({
+  appId: "doc/01775889-c700-413f-9b0e-6ba1837c52b0",
+  fields: ["Country", "Product Category", "=count(Customer)"]
+});
