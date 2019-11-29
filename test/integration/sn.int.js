2e4fdd2d426dfa29ac31d9802adf58a59b41eed8
Christoffer Åström <cam@qlik.com>
2019-11-29 16:54:42 +0100

    test: add `process.env.BASE_URL`

--- test/integration/sn.int.js
+++ test/integration/sn.int.js
@@ -2,7 +2,7 @@ describe('sn', () => {
   const content = '.nebulajs-sn';
   it('should say hello', async () => {
     const app = encodeURIComponent(process.env.APP_ID || '/apps/ctrl00.qvf');
-    await page.goto(`${process.testServer.url}/render/app/${app}`);
+    await page.goto(`${process.env.BASE_URL}/render/app/${app}`);
 
     await page.waitForSelector(content, { visible: true });
 
