2d6cc3b9fceed37992f801d3a1674514637fb52b
Christoffer Åström <stoffeastrom@gmail.com>
2019-12-06 08:47:07 +0100

    test: spice up `Footer` (#223)

---
+++ apis/nucleus/src/components/__tests__/footer.spec.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { create, act } from 'react-test-renderer';
+import { Typography } from '@material-ui/core';
+
+const [{ default: Footer }] = aw.mock([], ['../Footer']);
+
+describe('<Footer />', () => {
+  let sandbox;
+  let renderer;
+  let render;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    render = async layout => {
+      await act(async () => {
+        renderer = create(<Footer layout={layout} />);
+      });
+    };
+  });
+  afterEach(() => {
+    sandbox.restore();
+    renderer.unmount();
+  });
+  it('should render default', () => {
+    render();
+    expect(renderer.root.props.layout).to.be.an('undefined');
+  });
+  it('should render', () => {
+    render({ showTitles: true, footnote: 'foo' });
+    const types = renderer.root.findAllByType(Typography);
+    expect(types).to.have.length(1);
+    expect(types[0].props.children).to.equal('foo');
+  });
+});
