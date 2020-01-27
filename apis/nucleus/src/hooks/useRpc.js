6b2f2b34aac73b931ef0efb657eb0e41e00ec0b9
Christoffer Åström <cam@qlik.com>
2020-01-27 13:28:44 +0100

    fix(use-rpc): add back longrunning api

--- apis/nucleus/src/hooks/useRpc.js
+++ apis/nucleus/src/hooks/useRpc.js
@@ -19,6 +19,7 @@ const rpcReducer = (state, action) => {
         invalid: true,
         validating: true,
         canCancel: true,
+        rpcRetry: false,
       };
       break;
     }
@@ -33,6 +34,7 @@ const rpcReducer = (state, action) => {
         validating: false,
         canCancel: false,
         canRetry: false,
+        rpcRetry: false,
       };
       break;
     }
@@ -43,6 +45,7 @@ const rpcReducer = (state, action) => {
         validating: false,
         canRetry: true,
         canCancel: false,
+        rpcRetry: false,
       };
       break;
     }
@@ -52,10 +55,8 @@ const rpcReducer = (state, action) => {
   let sharedState = store.get(key);
   if (!sharedState) {
     sharedState = {};
-    // console.log('init state', action.model.handle);
   }
   sharedState[method] = newState;
-  // console.log(action.model.handle, method, sharedState, newState);
   store.set(key, sharedState);
   return newState;
 };
@@ -66,68 +67,79 @@ export default function useRpc(model, method) {
   const [state, dispatch] = useReducer(rpcReducer, key ? store.get(key) : null);
   const [modelChangedStore] = useModelChangedStore();
   const [requestStore] = useRpcRequestStore();
-  // console.log('userpc');
-  useEffect(() => {
-    if (!model) return undefined;
-    // const rpcKey = `${key}/${method}`;
-    // let rpc = requestStore.get(rpcKey);
-    let rpcShared = requestStore.get(key);
-    // console.log('rpcShared', rpcShared);
+
+  let rpcShared;
+
+  if (key) {
+    rpcShared = requestStore.get(key);
     if (!rpcShared) {
       rpcShared = {};
       requestStore.set(key, rpcShared);
-      // console.log('init rpc');
+    }
+  }
+
+  const call = async skipRetry => {
+    let cache = rpcShared[method];
+    if (!cache || (cache && cache.rpcRetry)) {
+      const rpc = model[method]();
+      cache = {
+        rpc,
+        rpcRetry: false,
+      };
+      rpcShared[method] = cache;
+
+      dispatch({
+        type: 'INVALID',
+        method,
+        key,
+        model,
+        store,
+        canCancel: true,
+      });
     }
 
-    const call = async skipRetry => {
-      let rpc = rpcShared[method];
-      if (!rpc) {
-        rpc = model[method]();
-        rpcShared[method] = rpc;
-
-        dispatch({
-          type: 'INVALID',
-          method,
-          key,
-          model,
-          store,
-          canCancel: true,
-        });
-      } else {
-        // console.log('ongoing');
-        // return;
-      }
-      try {
-        // await sleep(5000);
-        const result = await rpc;
-        dispatch({ type: 'VALID', result, key, method, model, store });
-      } catch (err) {
-        if (err.code === 15 && !skipRetry) {
-          // Request aborted. This will be called multiple times by hooks only retry once
-          // console.log('retry');
-          rpcShared[method] = null;
-          call(true);
+    try {
+      // await sleep(5000);
+      const result = await cache.rpc;
+      dispatch({ type: 'VALID', result, key, method, model, store });
+    } catch (err) {
+      if (err.code === 15 && !skipRetry) {
+        // Request aborted. This will be called multiple times by hooks only retry once
+        if (!cache.rpcRetry) {
+          cache.rpcRetry = true;
         }
+        call(true);
       }
-    };
+    }
+  };
+  const longrunning = {
+    cancel: async () => {
+      const global = model.session.getObjectApi({ handle: -1 });
+      await global.cancelRequest(rpcShared[method].rpc.requestId);
+      dispatch({
+        type: 'CANCELLED',
+        key,
+        method,
+        model,
+        store,
+      });
+    },
+    retry: () => call(),
+  };
+
+  useEffect(() => {
+    if (!model) return undefined;
+
     call();
 
     return undefined;
   }, [model, modelChangedStore.get(model && model.id), key, method]);
 
-  // console.log(model && model.handle, state);
-
   return [
     // Result
-    // state && state[method] && state[method].result,
     state && state.result,
-    // {
-    //   validating: state && state[method] && state[method].validating,
-    //   canCancel: state && state[method] && state[method].canCancel,
-    //   canRetry: state && state[method] && state[method].canRetry,
-    // },
     { validating: state && state.validating, canCancel: state && state.canCancel, canRetry: state && state.canRetry },
     // Long running api e.g cancel retry
-    // longrunning,
+    longrunning,
   ];
 }
