830cdb8aa8342fb0fb55abd2874199c7040fe078
Christoffer Åström <stoffeastrom@gmail.com>
2020-02-13 15:53:49 +0100

    feat: add shapes for svg icons (#312)
    BREAKING CHANGE: action definition changed

--- packages/ui/icons/down-arrow.js
+++ packages/ui/icons/down-arrow.js
@@ -2,8 +2,7 @@ import SvgIcon from './SvgIcon';
 
 const downArrow = props => ({
   ...props,
-  d: 'M8,9 L12.5,4.5 L14,6 L9.5,10.5 L8,12 L2,6 L3.5,4.5 L8,9 Z',
+  shapes: [{ type: 'path', attrs: { d: 'M8,9 L12.5,4.5 L14,6 L9.5,10.5 L8,12 L2,6 L3.5,4.5 L8,9 Z' } }],
 });
-
 export default props => SvgIcon(downArrow(props));
 export { downArrow };
