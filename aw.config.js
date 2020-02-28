4af3a40672ffa608692221a1999ab72dabcaa768
Christoffer Åström <stoffeastrom@gmail.com>
2020-02-28 09:08:21 +0100

    test: add `test-utils` (#333)

--- aw.config.js
+++ aw.config.js
@@ -9,6 +9,7 @@ module.exports = {
       '**/__stories__/**',
       '**/apis/supernova/index.js',
       '**/apis/nucleus/index.js',
+      '**/apis/test-utils/index.js',
       '**/packages/ui/icons/**/*.js', // Exclude the defined icons but test the `<SvgIcon />`
     ],
   },
