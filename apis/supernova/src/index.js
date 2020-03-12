d0f33eb14952973b7eafede68ba27ac32a5383bc
Christoffer Åström <stoffeastrom@gmail.com>
2020-03-12 13:40:29 +0100

    refactor: cleanup supernova exports (#352)

--- apis/supernova/src/index.js
+++ apis/supernova/src/index.js
@@ -4,47 +4,24 @@ import generator from './generator';
 export { generator };
 
 // core hooks
-export { hook } from './hooks';
-export { useState } from './hooks';
-export { useEffect } from './hooks';
-export { useMemo } from './hooks';
-export { useImperativeHandle } from './hooks';
+export { hook, useState, useEffect, useMemo, useImperativeHandle } from './hooks';
 
 // composed hooks
-export { usePromise } from './hooks';
-export { useAction } from './hooks';
-export { useRect } from './hooks';
-export { useModel } from './hooks';
-export { useApp } from './hooks';
-export { useGlobal } from './hooks';
-export { useElement } from './hooks';
-export { useSelections } from './hooks';
-export { useTheme } from './hooks';
-export { useLayout } from './hooks';
-export { useStaleLayout } from './hooks';
-export { useAppLayout } from './hooks';
-export { useTranslator } from './hooks';
-export { useConstraints } from './hooks';
-export { useOptions } from './hooks';
-
-export { onTakeSnapshot } from './hooks';
-
-/**
- * @typedef {object} GenericObjectLayout
- */
-
-/**
- * @typedef {object} NxAppLayout
- */
-
-/**
- * @interface EnigmaAppModel
- */
-
-/**
- * @interface EnigmaObjectModel
- */
-
-/**
- * @interface EnigmaGlobalModel
- */
+export {
+  usePromise,
+  useAction,
+  useRect,
+  useModel,
+  useApp,
+  useGlobal,
+  useElement,
+  useSelections,
+  useTheme,
+  useLayout,
+  useStaleLayout,
+  useAppLayout,
+  useTranslator,
+  useConstraints,
+  useOptions,
+  onTakeSnapshot,
+} from './hooks';
