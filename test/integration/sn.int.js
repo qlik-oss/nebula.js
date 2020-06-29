41ed560fb31280a3b03d524c87ff9f733837cc15
Miralem Drek <mek@qlik.com>
2020-06-29 13:17:39 +0200

    feat(cli-build): add json-parser

--- test/integration/sn.int.js
+++ test/integration/sn.int.js
@@ -1,12 +1,32 @@
-describe('sn', () => {
-  const content = '.njs-viz';
-  it('should say hello', async () => {
-    const app = encodeURIComponent(process.env.APP_ID || '/apps/ctrl00.qvf');
-    await page.goto(`${process.env.BASE_URL}/render/?app=${app}`);
+describe('Simple visualization', () => {
+  const content = '.simple-viz';
 
-    await page.waitForSelector(content, { visible: true });
+  describe('basic', () => {
+    before(async () => {
+      const app = encodeURIComponent(process.env.APP_ID || '/apps/ctrl00.qvf');
+      await page.goto(`${process.env.BASE_URL}/render/?app=${app}`);
 
-    const text = await page.$eval(content, (el) => el.textContent);
-    expect(text).to.equal('Hello engine!');
+      await page.waitForSelector(content, { visible: true });
+    });
+
+    it('should say hello', async () => {
+      const text = await page.$eval('.hello', (el) => el.textContent);
+      expect(text).to.equal('Hello engine!');
+    });
+
+    it('should be able to load json file', async () => {
+      const text = await page.$eval('.json-value', (el) => el.textContent);
+      expect(text).to.equal('Hi json!');
+    });
+
+    it('should be able to load css', async () => {
+      const bg = await page.$eval('.hello', (el) => window.getComputedStyle(el).backgroundColor);
+      expect(bg).to.equal('rgb(144, 41, 140)');
+    });
+
+    it('should have a dynamically calculated lavout value', async () => {
+      const text = await page.$eval('.layout-value', (el) => el.textContent);
+      expect(text).to.equal('3');
+    });
   });
 });
