2ce33628e54bc8c6e9cd82c393898944c1c78b70
Tobias Åström <tsm@qlik.com>
2020-11-16 13:12:45 +0100

    chore: add dev mashup example (#536)
    * chore: add dev mashup example
    
    * chore: update example readme
    
    * fix: update schema version
    
    * chore: change stardust to dep

---
+++ examples/dev-mashup/index.js
@@ -0,0 +1,36 @@
+import { embed } from '@nebula.js/stardust';
+import bar from '@nebula.js/sn-bar-chart';
+import line from '@nebula.js/sn-line-chart';
+import connect from './connect';
+
+function init() {
+  connect().then((app) => {
+    const nebbie = embed(app, {
+      types: [
+        {
+          name: 'bar',
+          load: () => Promise.resolve(bar),
+        },
+        {
+          name: 'line',
+          load: () => Promise.resolve(line),
+        },
+      ],
+    });
+
+    nebbie.selections().then((s) => s.mount(document.querySelector('.toolbar')));
+
+    document.querySelectorAll('.object').forEach((el) => {
+      const type = el.getAttribute('data-type');
+
+      nebbie.render({
+        type,
+        element: el,
+
+        fields: ['Alpha', '=Sum(Expression1)'],
+      });
+    });
+  });
+}
+
+init();
