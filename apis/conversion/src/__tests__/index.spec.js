f97fb476fcd1c7b056496084aa355b2bd5eb8b29
quanho <quan.ho@qlik.com>
2020-11-19 11:53:48 +0100

    refactor: refactor and add unit tests

--- apis/conversion/src/__tests__/index.spec.js
+++ apis/conversion/src/__tests__/index.spec.js
@@ -1,5 +1,4 @@
-import objectConversion from '../index';
-
+const doMock = ({ hypercube = {} } = {}) => aw.mock([['**/hypercube.js', () => hypercube]], ['../index.js']);
 describe('objectConversion', () => {
   describe('convertTo', () => {
     let sandbox;
@@ -10,11 +9,21 @@ describe('objectConversion', () => {
     let propertyTree;
     let sourceQae;
     let targetQae;
+    let hypercube;
+    let conversion;
 
     beforeEach(() => {
       sandbox = sinon.createSandbox();
-      sourceQae = {};
-      targetQae = {};
+      sourceQae = {
+        data: {
+          targets: [{ propertyPath: '/qHyperCubeDef' }],
+        },
+      };
+      targetQae = {
+        data: {
+          targets: [{ propertyPath: '/qHyperCubeDef' }],
+        },
+      };
       halo = {
         types: {
           get: sandbox.stub().returns({
@@ -24,7 +33,7 @@ describe('objectConversion', () => {
       };
       propertyTree = {
         qProperty: {
-          visualization: 'abc',
+          visualization: 'some type',
         },
       };
       model = { getFullPropertyTree: sandbox.stub().returns(propertyTree) };
@@ -33,94 +42,111 @@ describe('objectConversion', () => {
           getQae: sandbox.stub().returns(sourceQae),
         },
       };
+      hypercube = {
+        exportProperties: sandbox.stub().returns({ properties: { prop1: 'value 1' } }),
+        importProperties: sandbox.stub().returns({ qProperty: { prop2: 'value 2' } }),
+      };
+      [{ default: conversion }] = doMock({ hypercube });
     });
+
     afterEach(() => {
       sandbox.restore();
     });
-    it('should throw an error when there is no source exportProperties and no target importProperties', async () => {
-      newType = 'bar';
-      try {
-        await objectConversion.convertTo({ halo, model, cellRef, newType });
-        expect(cellRef.current.getQae.callCount).to.equal(1);
-        expect(model.getFullPropertyTree.callCount).to.equal(1);
-        expect(halo.types.get.callCount).to.equal(0);
-        expect(halo.types.get().supernova.callCount).to.equal(0);
-      } catch (err) {
-        expect(err.message).to.equal(
-          `Converting from ${propertyTree.qProperty.visualization} is not supported since there is no exportProperties method.`
-        );
-      }
-    });
 
-    it('should throw an error when there is no source exportProperties but target importProperties', async () => {
+    it('should use hypercube.exportProperties and hypercube.importProperties when there is no source exportProperties and no target importProperties', async () => {
       newType = 'bar';
-      targetQae = { importProperties: sandbox.stub().returns({ qProperty: { mnp: 10 } }) };
-      halo = {
-        types: {
-          get: sandbox.stub().returns({
-            supernova: sandbox.stub().returns({ qae: targetQae }),
-          }),
-        },
-      };
-      try {
-        await objectConversion.convertTo({ halo, model, cellRef, newType });
-        expect(cellRef.current.getQae.callCount).to.equal(1);
-        expect(model.getFullPropertyTree.callCount).to.equal(1);
-        expect(halo.types.get.callCount).to.equal(0);
-        expect(halo.types.get().supernova.callCount).to.equal(0);
-      } catch (err) {
-        expect(err.message).to.equal(
-          `Converting from ${propertyTree.qProperty.visualization} is not supported since there is no exportProperties method.`
-        );
-      }
-    });
-
-    it('should throw an error when there is source exportProperties but no target importProperties', async () => {
-      newType = 'bar';
-      sourceQae = { exportProperties: sandbox.stub().returns({ qProperty: { xyz: 1 } }) };
-      cellRef = {
-        current: {
-          getQae: sandbox.stub().returns(sourceQae),
-        },
-      };
-      try {
-        await objectConversion.convertTo({ halo, model, cellRef, newType });
-        expect(cellRef.current.getQae.callCount).to.equal(1);
-        expect(model.getFullPropertyTree.callCount).to.equal(1);
-        expect(halo.types.get.callCount).to.equal(1);
-        expect(halo.types.get().supernova.callCount).to.equal(1);
-      } catch (err) {
-        expect(err.message).to.equal(
-          `Converting to ${newType} is not supported since there is no importProperties method.`
-        );
-      }
-    });
-
-    it('should do convert correctly when there are source exportProperties and target importProperties', async () => {
-      newType = 'bar';
-      sourceQae = { exportProperties: sandbox.stub().returns({ qProperty: { xyz: 1 } }) };
-      cellRef = {
-        current: {
-          getQae: sandbox.stub().returns(sourceQae),
-        },
-      };
-      targetQae = { importProperties: sandbox.stub().returns({ qProperty: { mnp: 10 } }) };
-      halo = {
-        types: {
-          get: sandbox.stub().returns({
-            supernova: sandbox.stub().returns({ qae: targetQae }),
-          }),
-        },
-      };
-      const convertedTree = await objectConversion.convertTo({ halo, model, cellRef, newType });
+      const newPropertyTree = await conversion.convertTo({ halo, model, cellRef, newType });
       expect(cellRef.current.getQae.callCount).to.equal(1);
       expect(model.getFullPropertyTree.callCount).to.equal(1);
       expect(halo.types.get.callCount).to.equal(1);
       expect(halo.types.get().supernova.callCount).to.equal(1);
-      expect(convertedTree.qProperty.visualization).to.equal('bar');
-      expect(sourceQae.exportProperties.callCount).to.equal(1);
+      expect(hypercube.exportProperties.callCount).to.equal(1);
+      expect(hypercube.exportProperties).to.have.been.calledWithExactly({ propertyTree });
+      expect(hypercube.importProperties.callCount).to.equal(1);
+      expect(hypercube.importProperties).to.have.been.calledWithExactly({
+        exportFormat: { properties: { prop1: 'value 1' } },
+      });
+      expect(newPropertyTree).to.deep.equal({ qProperty: { prop2: 'value 2', visualization: 'bar' } });
+    });
+
+    it('should use hypercube.exportProperties when there is no source exportProperties', async () => {
+      newType = 'bar';
+      targetQae = { importProperties: sandbox.stub().returns({ qProperty: { prop4: 'value 4' } }) };
+      halo = {
+        types: {
+          get: sandbox.stub().returns({
+            supernova: sandbox.stub().returns({ qae: targetQae }),
+          }),
+        },
+      };
+      const newPropertyTree = await conversion.convertTo({ halo, model, cellRef, newType });
+      expect(cellRef.current.getQae.callCount).to.equal(1);
+      expect(model.getFullPropertyTree.callCount).to.equal(1);
+      expect(halo.types.get.callCount).to.equal(1);
+      expect(halo.types.get().supernova.callCount).to.equal(1);
+      expect(hypercube.exportProperties.callCount).to.equal(1);
+      expect(hypercube.exportProperties).to.have.been.calledWithExactly({ propertyTree });
+      expect(hypercube.importProperties.callCount).to.equal(0);
       expect(targetQae.importProperties.callCount).to.equal(1);
-      expect(convertedTree.qProperty.mnp).to.equal(10);
+      expect(targetQae.importProperties).to.have.been.calledWithExactly({
+        exportFormat: { properties: { prop1: 'value 1' } },
+      });
+      expect(newPropertyTree).to.deep.equal({ qProperty: { prop4: 'value 4', visualization: 'bar' } });
+    });
+
+    it('should use hypercube.importProperties when there is no target importProperties', async () => {
+      newType = 'bar';
+      sourceQae = { exportProperties: sandbox.stub().returns({ qProperty: { prop3: 'value 3' } }) };
+      cellRef = {
+        current: {
+          getQae: sandbox.stub().returns(sourceQae),
+        },
+      };
+      const newPropertyTree = await conversion.convertTo({ halo, model, cellRef, newType });
+      expect(cellRef.current.getQae.callCount).to.equal(1);
+      expect(model.getFullPropertyTree.callCount).to.equal(1);
+      expect(halo.types.get.callCount).to.equal(1);
+      expect(halo.types.get().supernova.callCount).to.equal(1);
+      expect(hypercube.exportProperties.callCount).to.equal(0);
+      expect(sourceQae.exportProperties.callCount).to.equal(1);
+      expect(sourceQae.exportProperties).to.have.been.calledWithExactly({ propertyTree });
+      expect(hypercube.importProperties.callCount).to.equal(1);
+      expect(hypercube.importProperties).to.have.been.calledWithExactly({
+        exportFormat: { qProperty: { prop3: 'value 3' } },
+      });
+      expect(newPropertyTree).to.deep.equal({ qProperty: { prop2: 'value 2', visualization: 'bar' } });
+    });
+
+    it('should do convert correctly when there are source exportProperties and target importProperties', async () => {
+      newType = 'bar';
+      sourceQae = { exportProperties: sandbox.stub().returns({ qProperty: { prop3: 'value 3' } }) };
+      cellRef = {
+        current: {
+          getQae: sandbox.stub().returns(sourceQae),
+        },
+      };
+      targetQae = { importProperties: sandbox.stub().returns({ qProperty: { prop4: 'value 4' } }) };
+      halo = {
+        types: {
+          get: sandbox.stub().returns({
+            supernova: sandbox.stub().returns({ qae: targetQae }),
+          }),
+        },
+      };
+      const newPropertyTree = await conversion.convertTo({ halo, model, cellRef, newType });
+      expect(cellRef.current.getQae.callCount).to.equal(1);
+      expect(model.getFullPropertyTree.callCount).to.equal(1);
+      expect(halo.types.get.callCount).to.equal(1);
+      expect(halo.types.get().supernova.callCount).to.equal(1);
+      expect(hypercube.exportProperties.callCount).to.equal(0);
+      expect(sourceQae.exportProperties.callCount).to.equal(1);
+      expect(sourceQae.exportProperties).to.have.been.calledWithExactly({ propertyTree });
+      expect(hypercube.importProperties.callCount).to.equal(0);
+      expect(targetQae.importProperties.callCount).to.equal(1);
+      expect(targetQae.importProperties).to.have.been.calledWithExactly({
+        exportFormat: { qProperty: { prop3: 'value 3' } },
+      });
+      expect(newPropertyTree).to.deep.equal({ qProperty: { prop4: 'value 4', visualization: 'bar' } });
     });
   });
 });
--- apis/conversion/src/index.js
+++ apis/conversion/src/index.js
@@ -11,21 +11,51 @@ const getType = async ({ halo, name, version }) => {
   return SN;
 };
 
+const getPath = (qae) => {
+  return qae && qae.data && qae.data.targets && qae.data.targets[0] && qae.data.targets[0].propertyPath;
+};
+
+const getDefaultExportPropertiesFnc = (path) => {
+  const steps = path.split('/');
+  if (steps.indexOf('qHyperCubeDef') > -1) {
+    return hypercube.exportProperties;
+  }
+  return undefined; // TODO: add listbox and other
+};
+
+const getExportPropertiesFnc = (qae) => {
+  if (qae.exportProperties) {
+    return qae.exportProperties;
+  }
+  const path = getPath(qae);
+  return getDefaultExportPropertiesFnc(path);
+};
+
+const getDefaultImportPropertiesFnc = (path) => {
+  const steps = path.split('/');
+  if (steps.indexOf('qHyperCubeDef') > -1) {
+    return hypercube.importProperties;
+  }
+  return undefined; // TODO: add listbox and other
+};
+
+const getImportPropertiesFnc = (qae) => {
+  if (qae.importProperties) {
+    return qae.importProperties;
+  }
+  const path = getPath(qae);
+  return getDefaultImportPropertiesFnc(path);
+};
+
 const convertTo = async ({ halo, model, cellRef, newType }) => {
   const propertyTree = await model.getFullPropertyTree();
   const sourceQae = cellRef.current.getQae();
-  if (!sourceQae.exportProperties) {
-    throw new Error(
-      `Converting from ${propertyTree.qProperty.visualization} is not supported since there is no exportProperties method.`
-    );
-  }
+  const exportProperties = getExportPropertiesFnc(sourceQae);
   const targetSnType = await getType({ halo, name: newType });
   const targetQae = targetSnType.qae;
-  if (!targetQae.importProperties) {
-    throw new Error(`Converting to ${newType} is not supported since there is no importProperties method.`);
-  }
-  const exportFormat = sourceQae.exportProperties({ propertyTree });
-  const newPropertyTree = targetQae.importProperties({ exportFormat });
+  const importProperties = getImportPropertiesFnc(targetQae);
+  const exportFormat = exportProperties({ propertyTree });
+  const newPropertyTree = importProperties({ exportFormat });
   newPropertyTree.qProperty.visualization = newType;
   return newPropertyTree;
 };
