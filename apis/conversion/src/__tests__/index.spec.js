0071d3c18c6571bcf0e8905e5fd8c6e48c492435
Quan Ho <quan.ho@qlik.com>
2020-11-30 14:20:47 +0100

    feat: add importProperties for hypercube conversion (#538)

--- apis/conversion/src/__tests__/index.spec.js
+++ apis/conversion/src/__tests__/index.spec.js
@@ -23,6 +23,9 @@ describe('objectConversion', () => {
         data: {
           targets: [{ propertyPath: '/qHyperCubeDef' }],
         },
+        properties: {
+          initial: {},
+        },
       };
       halo = {
         types: {
@@ -54,24 +57,32 @@ describe('objectConversion', () => {
     });
 
     it('should use hypercube.exportProperties and hypercube.importProperties when there is no source exportProperties and no target importProperties', async () => {
-      newType = 'bar';
+      newType = 'new type';
       const newPropertyTree = await convertTo({ halo, model, cellRef, newType });
       expect(cellRef.current.getQae.callCount).to.equal(1);
       expect(model.getFullPropertyTree.callCount).to.equal(1);
       expect(halo.types.get.callCount).to.equal(1);
       expect(halo.types.get().supernova.callCount).to.equal(1);
       expect(hypercube.exportProperties.callCount).to.equal(1);
-      expect(hypercube.exportProperties).to.have.been.calledWithExactly({ propertyTree });
+      expect(hypercube.exportProperties).to.have.been.calledWithExactly({ propertyTree, hypercubePath: '' });
       expect(hypercube.importProperties.callCount).to.equal(1);
       expect(hypercube.importProperties).to.have.been.calledWithExactly({
         exportFormat: { properties: { prop1: 'value 1' } },
+        initialProperties: { qInfo: { qType: 'new type' }, visualization: 'new type' },
+        dataDefinition: { propertyPath: '/qHyperCubeDef' },
+        hypercubePath: '',
       });
-      expect(newPropertyTree).to.deep.equal({ qProperty: { prop2: 'value 2', visualization: 'bar' } });
+      expect(newPropertyTree).to.deep.equal({ qProperty: { prop2: 'value 2' } });
     });
 
     it('should use hypercube.exportProperties when there is no source exportProperties', async () => {
-      newType = 'bar';
-      targetQae = { importProperties: sandbox.stub().returns({ qProperty: { prop4: 'value 4' } }) };
+      newType = 'new type';
+      targetQae = {
+        importProperties: sandbox.stub().returns({ qProperty: { prop4: 'value 4' } }),
+        properties: {
+          initial: {},
+        },
+      };
       halo = {
         types: {
           get: sandbox.stub().returns({
@@ -85,17 +96,20 @@ describe('objectConversion', () => {
       expect(halo.types.get.callCount).to.equal(1);
       expect(halo.types.get().supernova.callCount).to.equal(1);
       expect(hypercube.exportProperties.callCount).to.equal(1);
-      expect(hypercube.exportProperties).to.have.been.calledWithExactly({ propertyTree });
+      expect(hypercube.exportProperties).to.have.been.calledWithExactly({ propertyTree, hypercubePath: '' });
       expect(hypercube.importProperties.callCount).to.equal(0);
       expect(targetQae.importProperties.callCount).to.equal(1);
       expect(targetQae.importProperties).to.have.been.calledWithExactly({
         exportFormat: { properties: { prop1: 'value 1' } },
+        initialProperties: { qInfo: { qType: 'new type' }, visualization: 'new type' },
+        dataDefinition: {},
+        hypercubePath: '',
       });
-      expect(newPropertyTree).to.deep.equal({ qProperty: { prop4: 'value 4', visualization: 'bar' } });
+      expect(newPropertyTree).to.deep.equal({ qProperty: { prop4: 'value 4' } });
     });
 
     it('should use hypercube.importProperties when there is no target importProperties', async () => {
-      newType = 'bar';
+      newType = 'new type';
       sourceQae = { exportProperties: sandbox.stub().returns({ qProperty: { prop3: 'value 3' } }) };
       cellRef = {
         current: {
@@ -109,23 +123,31 @@ describe('objectConversion', () => {
       expect(halo.types.get().supernova.callCount).to.equal(1);
       expect(hypercube.exportProperties.callCount).to.equal(0);
       expect(sourceQae.exportProperties.callCount).to.equal(1);
-      expect(sourceQae.exportProperties).to.have.been.calledWithExactly({ propertyTree });
+      expect(sourceQae.exportProperties).to.have.been.calledWithExactly({ propertyTree, hypercubePath: '' });
       expect(hypercube.importProperties.callCount).to.equal(1);
       expect(hypercube.importProperties).to.have.been.calledWithExactly({
         exportFormat: { qProperty: { prop3: 'value 3' } },
+        initialProperties: { qInfo: { qType: 'new type' }, visualization: 'new type' },
+        dataDefinition: { propertyPath: '/qHyperCubeDef' },
+        hypercubePath: '',
       });
-      expect(newPropertyTree).to.deep.equal({ qProperty: { prop2: 'value 2', visualization: 'bar' } });
+      expect(newPropertyTree).to.deep.equal({ qProperty: { prop2: 'value 2' } });
     });
 
     it('should do convert correctly when there are source exportProperties and target importProperties', async () => {
-      newType = 'bar';
+      newType = 'new type';
       sourceQae = { exportProperties: sandbox.stub().returns({ qProperty: { prop3: 'value 3' } }) };
       cellRef = {
         current: {
           getQae: sandbox.stub().returns(sourceQae),
         },
       };
-      targetQae = { importProperties: sandbox.stub().returns({ qProperty: { prop4: 'value 4' } }) };
+      targetQae = {
+        importProperties: sandbox.stub().returns({ qProperty: { prop4: 'value 4' } }),
+        properties: {
+          initial: {},
+        },
+      };
       halo = {
         types: {
           get: sandbox.stub().returns({
@@ -140,13 +162,16 @@ describe('objectConversion', () => {
       expect(halo.types.get().supernova.callCount).to.equal(1);
       expect(hypercube.exportProperties.callCount).to.equal(0);
       expect(sourceQae.exportProperties.callCount).to.equal(1);
-      expect(sourceQae.exportProperties).to.have.been.calledWithExactly({ propertyTree });
+      expect(sourceQae.exportProperties).to.have.been.calledWithExactly({ propertyTree, hypercubePath: '' });
       expect(hypercube.importProperties.callCount).to.equal(0);
       expect(targetQae.importProperties.callCount).to.equal(1);
       expect(targetQae.importProperties).to.have.been.calledWithExactly({
         exportFormat: { qProperty: { prop3: 'value 3' } },
+        initialProperties: { qInfo: { qType: 'new type' }, visualization: 'new type' },
+        dataDefinition: {},
+        hypercubePath: '',
       });
-      expect(newPropertyTree).to.deep.equal({ qProperty: { prop4: 'value 4', visualization: 'bar' } });
+      expect(newPropertyTree).to.deep.equal({ qProperty: { prop4: 'value 4' } });
     });
   });
 });
--- apis/conversion/src/helpers/__tests__/index.spec.js
+++ apis/conversion/src/helpers/__tests__/index.spec.js
@@ -46,4 +46,343 @@ describe('helpers', () => {
       });
     });
   });
+
+  describe('isMasterItemPropperty', () => {
+    it('should return true for qMetaDef', () => {
+      expect(helpers.isMasterItemPropperty('qMetaDef')).to.equal(true);
+    });
+
+    it('should return true for descriptionExpression', () => {
+      expect(helpers.isMasterItemPropperty('descriptionExpression')).to.equal(true);
+    });
+
+    it('should return true for labelExpression', () => {
+      expect(helpers.isMasterItemPropperty('labelExpression')).to.equal(true);
+    });
+
+    it('should return false for everything else', () => {
+      expect(helpers.isMasterItemPropperty('qMetaDef1')).to.equal(false);
+      expect(helpers.isMasterItemPropperty('descriptionExpression1')).to.equal(false);
+      expect(helpers.isMasterItemPropperty('labelExpression1')).to.equal(false);
+      expect(helpers.isMasterItemPropperty('abc')).to.equal(false);
+    });
+  });
+
+  describe('importCommonProperties', () => {
+    let newProperties;
+    let exportFormat;
+    let initialProperties;
+    describe('visualization', () => {
+      beforeEach(() => {
+        newProperties = {};
+        exportFormat = {};
+        initialProperties = {
+          visualization: 'some visualization',
+        };
+      });
+
+      it('should copy visualization correctly', () => {
+        helpers.importCommonProperties(newProperties, exportFormat, initialProperties);
+        expect(newProperties.visualization).to.equal('some visualization');
+      });
+    });
+
+    describe('qType', () => {
+      beforeEach(() => {
+        newProperties = {};
+        exportFormat = {};
+        initialProperties = {
+          qInfo: { qType: 'some type' },
+        };
+      });
+
+      it('should set qType correctly if it is not a master object', () => {
+        helpers.importCommonProperties(newProperties, exportFormat, initialProperties);
+        expect(newProperties.qInfo.qType).to.equal('some type');
+      });
+
+      it('should set qType correctly if it is a master object', () => {
+        exportFormat.properties = { qInfo: { qType: 'masterobject' } };
+        helpers.importCommonProperties(newProperties, exportFormat, initialProperties);
+        expect(newProperties.qInfo.qType).to.equal('masterobject');
+      });
+    });
+  });
+
+  describe('copyPropertyIfExist', () => {
+    let source;
+    let target;
+    beforeEach(() => {
+      source = {
+        prop1: 1,
+        prop2: 2,
+        prop3: undefined,
+      };
+      target = {
+        prop1: 10,
+        prop3: 30,
+        prop4: 40,
+      };
+    });
+
+    it('should copy property if it exists in source to target', () => {
+      helpers.copyPropertyIfExist('prop1', source, target);
+      helpers.copyPropertyIfExist('prop2', source, target);
+      helpers.copyPropertyIfExist('prop3', source, target);
+      helpers.copyPropertyIfExist('prop4', source, target);
+      helpers.copyPropertyIfExist('prop5', source, target);
+
+      expect(target).to.deep.equal({
+        prop1: 1,
+        prop2: 2,
+        prop3: undefined,
+        prop4: 40,
+      });
+    });
+  });
+
+  describe('copyPropertyOrSetDefault', () => {
+    let source;
+    let target;
+    beforeEach(() => {
+      source = {
+        prop1: 1,
+        prop2: 2,
+        prop3: undefined,
+      };
+      target = {
+        prop1: 10,
+        prop3: 30,
+        prop4: 40,
+      };
+    });
+
+    it('should copy property if it exists in source to target, otherwise set to default', () => {
+      helpers.copyPropertyOrSetDefault('prop1', source, target);
+      helpers.copyPropertyOrSetDefault('prop2', source, target);
+      helpers.copyPropertyOrSetDefault('prop3', source, target);
+      helpers.copyPropertyOrSetDefault('prop4', source, target);
+      helpers.copyPropertyOrSetDefault('prop5', source, target);
+      helpers.copyPropertyOrSetDefault('prop6', source, target, 60);
+
+      expect(target).to.deep.equal({
+        prop1: 1,
+        prop2: 2,
+        prop3: undefined,
+        prop4: undefined,
+        prop5: undefined,
+        prop6: 60,
+      });
+    });
+  });
+
+  describe('createDefaultDimension', () => {
+    let dimensionDef;
+    let dimensionProperties;
+    beforeEach(() => {
+      dimensionDef = {
+        prop1: 1,
+        prop2: 2,
+      };
+      dimensionProperties = {
+        prop1: 10,
+        prop3: 30,
+      };
+    });
+
+    it('should add default values if there is no qOtherTotalSpec and othersLabel', () => {
+      const def = helpers.createDefaultDimension(dimensionDef, dimensionProperties);
+
+      expect(def).to.deep.equal({
+        prop1: 1,
+        prop2: 2,
+        prop3: 30,
+        qOtherTotalSpec: {
+          qOtherCounted: { qv: '10' },
+          qOtherLimit: { qv: '0' },
+        },
+        othersLabel: 'Others',
+      });
+    });
+
+    it('should keep othersLabel', () => {
+      dimensionProperties.othersLabel = 'some label';
+      const def = helpers.createDefaultDimension(dimensionDef, dimensionProperties);
+
+      expect(def).to.deep.equal({
+        prop1: 1,
+        prop2: 2,
+        prop3: 30,
+        qOtherTotalSpec: {
+          qOtherCounted: { qv: '10' },
+          qOtherLimit: { qv: '0' },
+        },
+        othersLabel: 'some label',
+      });
+    });
+
+    it('should keep qOtherCounted', () => {
+      dimensionProperties.qOtherTotalSpec = { qOtherCounted: { qv: '20' } };
+      const def = helpers.createDefaultDimension(dimensionDef, dimensionProperties);
+
+      expect(def).to.deep.equal({
+        prop1: 1,
+        prop2: 2,
+        prop3: 30,
+        qOtherTotalSpec: {
+          qOtherCounted: { qv: '20' },
+          qOtherLimit: { qv: '0' },
+        },
+        othersLabel: 'Others',
+      });
+    });
+
+    it('should keep qOtherLimit', () => {
+      dimensionProperties.qOtherTotalSpec = { qOtherLimit: { qv: '30' } };
+      const def = helpers.createDefaultDimension(dimensionDef, dimensionProperties);
+
+      expect(def).to.deep.equal({
+        prop1: 1,
+        prop2: 2,
+        prop3: 30,
+        qOtherTotalSpec: {
+          qOtherCounted: { qv: '10' },
+          qOtherLimit: { qv: '30' },
+        },
+        othersLabel: 'Others',
+      });
+    });
+
+    it('should keep qOtherCounted, qOtherLimit, and othersLabel', () => {
+      dimensionProperties.othersLabel = 'some label';
+      dimensionProperties.qOtherTotalSpec = {
+        qOtherCounted: { qv: '20' },
+        qOtherLimit: { qv: '30' },
+      };
+      const def = helpers.createDefaultDimension(dimensionDef, dimensionProperties);
+
+      expect(def).to.deep.equal({
+        prop1: 1,
+        prop2: 2,
+        prop3: 30,
+        qOtherTotalSpec: {
+          qOtherCounted: { qv: '20' },
+          qOtherLimit: { qv: '30' },
+        },
+        othersLabel: 'some label',
+      });
+    });
+  });
+
+  describe('createDefaultMeasure', () => {
+    let measureDef;
+    let measureProperties;
+    beforeEach(() => {
+      measureDef = {
+        prop1: 1,
+        prop2: 2,
+      };
+      measureProperties = {
+        prop1: 10,
+        prop3: 30,
+      };
+    });
+
+    it('should create default measure correctly', () => {
+      const def = helpers.createDefaultMeasure(measureDef, measureProperties);
+
+      expect(def).to.deep.equal({
+        prop1: 1,
+        prop2: 2,
+        prop3: 30,
+      });
+    });
+  });
+
+  describe('resolveValue', () => {
+    let data;
+    let input;
+
+    beforeEach(() => {
+      data = (a) => a * 2;
+      input = 5;
+    });
+
+    it('should resolve value correctly if data is a function', () => {
+      expect(helpers.resolveValue(data, input)).to.equal(10);
+      expect(helpers.resolveValue(data, input, 100)).to.equal(10);
+    });
+
+    it('should resolve value correctly if data is a number', () => {
+      data = 20;
+      expect(helpers.resolveValue(data, input)).to.equal(20);
+      expect(helpers.resolveValue(data, input, 100)).to.equal(20);
+    });
+
+    it('should resolve value correctly if data is not a function or not a number', () => {
+      expect(helpers.resolveValue(NaN, input)).to.equal(undefined);
+      expect(helpers.resolveValue(NaN, input, 100)).to.equal(100);
+      expect(helpers.resolveValue('a', input)).to.equal(undefined);
+      expect(helpers.resolveValue('a', input, 100)).to.equal(100);
+    });
+  });
+
+  describe('getHypercubePath', () => {
+    let qae;
+
+    beforeEach(() => {
+      qae = {
+        data: { targets: [{ propertyPath: '/qHyperCubeDef' }] },
+      };
+    });
+
+    it('should return to empty string if propertyPath = "/qHyperCubeDef"', () => {
+      expect(helpers.getHypercubePath(qae)).to.equal('');
+    });
+
+    it('should return to the part before /qHyperCubeDef', () => {
+      qae.data.targets[0].propertyPath = 'boxPlotDef/qHyperCubeDef';
+      expect(helpers.getHypercubePath(qae)).to.equal('boxPlotDef');
+    });
+
+    it('should return to the part before /qHyperCubeDef and replace / by .', () => {
+      qae.data.targets[0].propertyPath = 'boxPlotDef/point/qHyperCubeDef';
+      expect(helpers.getHypercubePath(qae)).to.equal('boxPlotDef.point');
+    });
+  });
+
+  describe('getDefaultDimension', () => {
+    it('should return correct default dimension', () => {
+      expect(helpers.getDefaultDimension()).to.deep.equal({
+        qDef: {
+          autoSort: true,
+          cId: '',
+          othersLabel: 'Others',
+        },
+        qLibraryId: '',
+        qNullSuppression: false,
+        qOtherLabel: 'Others',
+        qOtherTotalSpec: {
+          qOtherLimitMode: 'OTHER_GE_LIMIT',
+          qOtherMode: 'OTHER_OFF',
+          qOtherSortMode: 'OTHER_SORT_DESCENDING',
+          qSuppressOther: false,
+        },
+      });
+    });
+  });
+
+  describe('getDefaultMeasure', () => {
+    it('should return correct default measure', () => {
+      expect(helpers.getDefaultMeasure()).to.deep.equal({
+        qDef: {
+          autoSort: true,
+          cId: '',
+          numFormatFromTemplate: true,
+        },
+        qLibraryId: '',
+        qTrendLines: [],
+      });
+    });
+  });
 });
