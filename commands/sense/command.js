a425578a674bba5688d5207ba06a5930f881bb13
Tobias Linsefors <tobias.linsefors@qlik.com>
2021-07-01 13:55:16 +0200

    fix(cli-sense): apply default when using javascript api

--- commands/sense/command.js
+++ commands/sense/command.js
@@ -1,51 +1,13 @@
-const buildLegacy = require('./lib/build-legacy');
-const build = require('./lib/build');
+const init = require('./lib/init-config');
+const sense = require('./lib/sense');
 
 module.exports = {
   command: 'sense',
   desc: 'Build a nebula visualization as a Qlik Sense extension',
   builder(yargs) {
-    yargs.option('ext', {
-      type: 'string',
-      required: false,
-      desc: 'Extension definition',
-    });
-    yargs.option('meta', {
-      type: 'string',
-      required: false,
-      desc: 'Extension meta information',
-    });
-    yargs.option('output', {
-      type: 'string',
-      required: false,
-      default: '<name>-ext',
-      desc: 'Destination directory',
-    });
-    yargs.option('minify', {
-      type: 'boolean',
-      required: false,
-      default: true,
-      desc: 'Minify and uglify code',
-    });
-    yargs.option('sourcemap', {
-      type: 'boolean',
-      required: false,
-      default: false,
-      desc: 'Generate sourcemaps',
-    });
-    yargs.option('partial', {
-      type: 'boolean',
-      required: false,
-      default: false,
-      desc: 'Generate partial extension',
-      hidden: true,
-    });
+    init(yargs);
   },
   handler(argv) {
-    if (argv.partial) {
-      build(argv);
-    } else {
-      buildLegacy(argv);
-    }
+    sense(argv);
   },
 };
