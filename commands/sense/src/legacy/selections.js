f70c575ecab3916e89bd4d5dbc7c5b76b1afe11e
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2021-08-13 14:18:45 +0200

    fix: use same event in extensionwrapper (#651)

--- commands/sense/src/legacy/selections.js
+++ commands/sense/src/legacy/selections.js
@@ -60,6 +60,7 @@ export default (scope) => {
   scope.selectionsApi.confirm = () => {
     scope.backendApi.endSelections(true).then(() => {
       scope.selectionsApi.deactivated();
+      selectionAPI.emit('confirmed');
       selectionAPI.emit('deactivated');
     });
   };
@@ -74,6 +75,7 @@ export default (scope) => {
     scope.backendApi.endSelections(false);
     scope.selectionsApi.deactivated();
     selectionAPI.emit('canceled');
+    selectionAPI.emit('deactivated');
   };
 
   return selectionAPI;
