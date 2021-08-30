052c0c24819419f2fca873eba4506c26bcd3d776
caele <tsm@qlik.com>
2021-08-30 13:24:38 +0200

    chore: log test steps

--- commands/create/templates/sn/picasso/barchart/test/integration/interaction.int.js
+++ commands/create/templates/sn/picasso/barchart/test/integration/interaction.int.js
@@ -2,18 +2,24 @@ describe('interaction', () => {
   const content = '.njs-viz[data-render-count="1"]';
   it('should select two bars', async () => {
     const app = encodeURIComponent(process.env.APP_ID || '/apps/ctrl00.qvf');
+    console.log('START');
     await page.goto(`${process.env.BASE_URL}/render/?app=${app}&cols=Alpha,=5+avg(Expression1)`);
+    console.log('1');
     await page.waitForSelector(content, { visible: true });
-
+    console.log('2');
     await page.click('rect[data-label="K"]');
+    console.log('3');
     await page.click('rect[data-label="S"]');
-
+    console.log('4');
     await page.waitForSelector('button[title="Confirm selection"]:not([disabled])', { visible: true });
+    console.log('5');
     await page.click('button[title="Confirm selection"]');
+    console.log('6');
 
     await page.waitForFunction((selector) => document.querySelectorAll(selector).length === 2, {}, 'rect[data-label]');
-
+    console.log('7');
     const rects = await page.$$eval('rect[data-label]', (sel) => sel.map((r) => r.getAttribute('data-label')));
+    console.log('8');
     expect(rects).to.eql(['K', 'S']);
   });
 });
