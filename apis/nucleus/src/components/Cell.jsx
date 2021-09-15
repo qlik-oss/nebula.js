6034487f7488ab7605e418824d1af8f33bb6afd9
caele <tsm@qlik.com>
2021-09-15 16:35:43 +0200

    Revert "proto: allow manual keyboard option"
    This reverts commit 36f647e8d75f7a49231eb97d70a0d5fb65628b94.

--- apis/nucleus/src/components/Cell.jsx
+++ apis/nucleus/src/components/Cell.jsx
@@ -467,14 +467,14 @@ const Cell = forwardRef(
     return (
       <Paper
         style={{ position: 'relative', width: '100%', height: '100%', overflow: 'hidden' }}
-        tabIndex={keyboardNavigation === true ? 0 : -1}
+        tabIndex={keyboardNavigation ? 0 : -1}
         elevation={0}
         square
         className={CellElement.className}
         ref={cellRef}
         onMouseEnter={handleOnMouseEnter}
         onMouseLeave={handleOnMouseLeave}
-        onKeyDown={keyboardNavigation === true ? handleKeyDown : null}
+        onKeyDown={keyboardNavigation ? handleKeyDown : null}
       >
         <Grid
           container
