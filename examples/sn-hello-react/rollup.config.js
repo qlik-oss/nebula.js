debcdfb8eac08f51f613baaabddbfcc4c1cd9b3e
caele <tsm@qlik.com>
2021-09-21 10:22:00 +0200

    chore: update react example

--- examples/sn-hello-react/rollup.config.js
+++ examples/sn-hello-react/rollup.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
+const babel = require('@rollup/plugin-babel');
 const commonjs = require('@rollup/plugin-commonjs');
-const babel = require('rollup-plugin-babel');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const postcss = require('rollup-plugin-postcss');
 const replace = require('@rollup/plugin-replace');
