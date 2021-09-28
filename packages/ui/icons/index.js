4b2bd2ff1210670e30217c678984f2c83ad49637
caele <tsm@qlik.com>
2021-09-28 16:55:10 +0200

    chore: update icons

--- packages/ui/icons/index.js
+++ packages/ui/icons/index.js
@@ -1,11 +1,11 @@
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import Brightness3 from '@material-ui/icons/Brightness3';
-import WbSunny from '@material-ui/icons/WbSunny';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import ColorLens from '@material-ui/icons/ColorLens';
-import Language from '@material-ui/icons/Language';
-import Help from '@material-ui/icons/Help';
-import Home from '@material-ui/icons/Home';
+import ChevronRight from '@mui/icons-material/ChevronRight';
+import ChevronLeft from '@mui/icons-material/ChevronLeft';
+import Brightness3 from '@mui/icons-material/Brightness3';
+import WbSunny from '@mui/icons-material/WbSunny';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import ColorLens from '@mui/icons-material/ColorLens';
+import Language from '@mui/icons-material/Language';
+import Help from '@mui/icons-material/Help';
+import Home from '@mui/icons-material/Home';
 
 export { ChevronRight, ChevronLeft, Brightness3, WbSunny, ExpandMore, ColorLens, Language, Help, Home };
