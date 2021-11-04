ab867281d74c669fc1d8ee59cfe46f6d7ffdef27
Max Gefvert <max.gefvert@qlik.com>
2021-11-04 08:15:41 +0100

    chore: setup babel for aw

--- aw.config.js
+++ aw.config.js
@@ -3,6 +3,22 @@ module.exports = {
     timeout: 30000,
   },
   mocks: [],
+  babel: {
+    options: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@babel/preset-react',
+      ],
+      // plugins: ['@babel/plugin-syntax-dynamic-import'],
+    },
+  },
   nyc: {
     exclude: [
       '**/commands/**',
