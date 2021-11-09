ada5f686e3cae4ba2443065043fa92ec07d1a061
Thomas <thomas.bengtsson@qlik.com>
2021-11-09 08:32:06 +0100

    feat(engima-mock): improve mock flexibility

--- apis/enigma-mock/src/__tests__/index.spec.js
+++ apis/enigma-mock/src/__tests__/index.spec.js
@@ -1,7 +1,7 @@
 import createEnigmaMock from '..';
 
 const fixture = {
-  layout: {
+  getLayout: {
     qHyperCube: {
       qDataPages: [],
     },
@@ -19,7 +19,7 @@ describe('enigma-mock', () => {
     expect(app.id).to.be.a.string;
   });
 
-  it('throws if not fixture is specified', async () => {
+  it('throws if no fixture is specified', async () => {
     expect(createEnigmaMock).to.throw();
   });
 
@@ -50,47 +50,8 @@ describe('enigma-mock', () => {
         const { app } = await createEnigmaMock(fixture);
         const object = await app.getObject();
         const layout = await object.getLayout();
-        expect(layout).to.equal(fixture.layout);
+        expect(layout).to.equal(fixture.getLayout);
       });
-    });
-  });
-
-  describe('override', () => {
-    it('override id of app', async () => {
-      const overrides = () => ({
-        id: 'custom-app-id',
-      });
-      const { app } = await createEnigmaMock(fixture, { overrides });
-      expect(app.id).to.equal('custom-app-id');
-    });
-
-    it('override getObject', async () => {
-      const overrides = ({ base }) => ({
-        getObject(id) {
-          if (/^ColorMapModel/.test(id)) {
-            return Promise.resolve({
-              id: 'colors',
-              getLayout: () => Promise.resolve({ colorMap: [{ id: 'color1' }] }),
-            });
-          }
-          return Promise.resolve(base.getObject());
-        },
-      });
-      const { app } = await createEnigmaMock(fixture, { overrides });
-      const colorObject = await app.getObject('ColorMapModel');
-      const colorLayout = await colorObject.getLayout();
-      expect(colorLayout).to.include.keys('colorMap');
-
-      const otherObject = await app.getObject('other');
-      const otherLayout = await otherObject.getLayout();
-      expect(otherLayout).to.equal(fixture.layout);
-    });
-
-    it('should be called with fixture and base as arguments', async () => {
-      const overrides = sinon.stub().returns({});
-      const { base } = await createEnigmaMock(fixture, { overrides });
-
-      expect(overrides).to.have.been.calledWith({ fixture, base });
     });
   });
 });
--- apis/enigma-mock/src/index.js
+++ apis/enigma-mock/src/index.js
@@ -1,22 +1,67 @@
 import spy from './spy';
 
-function getHyperCubeData(key, pages, meta) {
-  if (/qAttributeDimensions/.test(key)) {
-    return meta.attrDimDataPages;
+/**
+ * Get value for a fixture property.
+ *
+ * The value is either static (e.g. pass a string / object / similar) or dynamic when passing a function.
+ *
+ * It falls back to the default value in case the fixture has no value specified.
+ *
+ * @param {any} value Fixture value. Either a static value (string / object / boolean / ...) or a function invoked when the value is needed.
+ * @param {object} options Options.
+ * @param {Array<any>} options.args Arguments used to invoke value function
+ * @param {any} options.defaultValu Default value in case value in fixture is undefined.
+ *
+ * @returns The value.
+ */
+const getValue = (value, { args = [], defaultValue } = {}) => {
+  if (typeof value === 'function') {
+    return value(...args);
+  }
+  if (value !== undefined) {
+    return value;
+  }
+  return defaultValue;
+};
+
+function GetObjectMock(fixture) {
+  // TODO Add more validations
+  if (!fixture.getLayout) {
+    throw new Error('Fixture is missing "getLayout"');
   }
 
-  return meta.layout.qHyperCube.qDataPages;
+  return {
+    id: getValue(fixture.id, { defaultValue: `object - ${+Date.now()}` }),
+    on: () => {},
+    once: () => {},
+    getEffectiveProperties: (...args) =>
+      Promise.resolve(getValue(fixture.getEffectiveProperties, { args, defaultValue: {} })),
+    session: getValue(fixture.session, { defaultValue: true }),
+    getLayout: (...args) => Promise.resolve(getValue(fixture.getLayout, args)),
+    selectHyperCubeValues: (...args) =>
+      Promise.resolve(getValue(fixture.selectHyperCubeValues, { args, defaultValue: true })),
+    rangeSelectHyperCubeValues: (...args) =>
+      Promise.resolve(getValue(fixture.rangeSelectHyperCubeValues, { args, defaultValue: true })),
+    beginSelections: (...args) => Promise.resolve(getValue(fixture.beginSelections, { args, defaultValue: true })),
+    resetMadeSelections: (...args) =>
+      Promise.resolve(getValue(fixture.resetMadeSelections, { args, defaultValue: true })),
+    getHyperCubeTreeData: (...args) =>
+      Promise.resolve(getValue(fixture.getHyperCubeTreeData, { args, defaultValue: [] })),
+    getHyperCubeData: (...args) => Promise.resolve(getValue(fixture.getHyperCubeData, { args, defaultValue: [] })),
+    getHyperCubeReducedData: (...args) => Promise.resolve(getValue(fixture.getHyperCubeReducedData, { args })),
+    getHyperCubeContinuousData: (...args) => Promise.resolve(getValue(fixture.getHyperCubeContinuousData, { args })),
+    getHyperCubeStackData: (...args) => Promise.resolve(getValue(fixture.getHyperCubeStackData, { args })),
+  };
 }
 
-export default (fixture, { overrides } = {}) => {
+export default (fixture) => {
   if (!fixture) {
     throw new Error('No "fixture" specified');
   }
-  const { layout, reducedDataPages } = fixture;
 
-  const redPages = reducedDataPages && reducedDataPages.length > 0 ? reducedDataPages : layout.qHyperCube.qDataPages;
+  const getObjectMock = new GetObjectMock(fixture);
 
-  const base = {
+  const app = {
     id: `app - ${+Date.now()}`,
     session: {
       getObjectApi() {
@@ -35,34 +80,12 @@ export default (fixture, { overrides } = {}) => {
       });
     },
     getObject() {
-      return Promise.resolve(
-        spy({
-          id: `object - ${+Date.now()}`,
-          on: () => {},
-          once: () => {},
-          getEffectiveProperties: () => Promise.resolve({}),
-          session: true,
-          getHyperCubeTreeData: () => Promise.resolve(layout.qHyperCube.qTreeDataPages),
-          getHyperCubeData: (key, pages) => Promise.resolve(getHyperCubeData(key, pages, fixture)),
-          getHyperCubeReducedData: () => Promise.resolve(redPages),
-          getHyperCubeContinuousData: () =>
-            Promise.resolve({ qAxisData: layout.qHyperCube.qAxisData, qDataPages: layout.qHyperCube.qDataPages }),
-          getHyperCubeStackData: () => Promise.resolve(layout.qHyperCube.qStackedDataPages),
-          getLayout: () => Promise.resolve(layout),
-          selectHyperCubeValues: () => Promise.resolve(true),
-          rangeSelectHyperCubeValues: () => Promise.resolve(true),
-          beginSelections: () => Promise.resolve(true),
-          resetMadeSelections: () => Promise.resolve(true),
-        })
-      );
+      return Promise.resolve(spy(getObjectMock));
     },
     getAppLayout() {
       return Promise.resolve({ id: 'app-layout' });
     },
   };
 
-  const appOverrides = overrides ? overrides({ fixture, base }) : {};
-  const app = { ...base, ...appOverrides };
-
-  return Promise.resolve({ app, base, spy });
+  return Promise.resolve({ app, spy });
 };
