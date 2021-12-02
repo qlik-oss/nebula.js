d9c8be0d6fceb726d8a25274f259234f782a6309
Thomas <thomas.bengtsson@qlik.com>
2021-12-02 16:13:36 +0100

    test: migrate to use Enigma Mocker

--- test/mashup/visualize/life.int.js
+++ test/mashup/visualize/life.int.js
@@ -1,4 +1,4 @@
-describe.skip('object lifecycle', () => {
+describe('object lifecycle', () => {
   const waitForTextStatus = async (selector, text, options = { timeout: 1000 }) => {
     await page.waitForSelector(selector, { visible: true });
     await page.waitForFunction(
@@ -8,13 +8,14 @@ describe.skip('object lifecycle', () => {
       text
     );
   };
-  before(async () => {
-    await page.goto(`${process.env.BASE_URL}/visualize/life.html`);
-    await page.waitForSelector('button[data-phase="init-as-bad-type"]', { visible: true });
-  });
+
+  function getScenarioUrl(scenario) {
+    return `${process.env.BASE_URL}/visualize/life.html?scenario=${scenario}`;
+  }
 
   it('should fail to load unknown type', async () => {
-    await page.click('button[data-phase="init-as-bad-type"]');
+    const url = getScenarioUrl('invalid-type');
+    await page.goto(url);
     await waitForTextStatus(
       '[data-tid="error-title"]',
       "Could not find a version of 'voooz' that supports current object version. Did you forget to register voooz?"
@@ -22,7 +23,8 @@ describe.skip('object lifecycle', () => {
   });
 
   it('should show spinner and requirements for known type', async () => {
-    await page.click('button[data-phase="set-proper-type"]');
+    const url = getScenarioUrl('valid-type');
+    await page.goto(url);
 
     // should show loading spinner
     await page.waitForSelector('.njs-cell [role="progressbar"]', { timeout: 3000 });
@@ -32,40 +34,27 @@ describe.skip('object lifecycle', () => {
 
   // need to fix calc condition view first
   it('should show calculation unfulfilled', async () => {
-    await page.click('button[data-phase="set-calc-condition"]');
+    const url = getScenarioUrl('calc-unfulfilled');
+    await page.goto(url);
     await waitForTextStatus('[data-tid="error-title"]', 'The calculation condition is not fulfilled');
   });
 
   it('should render error when a hypercube contains a qError', async () => {
-    await page.click('button[data-phase="hypercube-error"]');
+    const url = getScenarioUrl('hypercube-error');
+    await page.goto(url);
     await waitForTextStatus('[data-tid="error-title"]', 'Error');
   });
 
   it('should render when requirements are fulfilled', async () => {
-    await page.click('button[data-phase="fulfill-requirements"]');
+    const url = getScenarioUrl('valid-config');
+    await page.goto(url);
     await waitForTextStatus('.rendered', 'Success!');
   });
 
-  it('should render long running query', async () => {
-    await page.click('button[data-phase="long-running-query"]');
-
-    // the cancel button should appear after 2000ms
-    // TODO - fix error thrown when long running query is cancelled
-    // await waitForTextStatus('.njs-cell button', 'Cancel', { timeout: 2500 });
-    // await page.click('.njs-cell button');
-    // await waitForTextStatus('.njs-cell button', 'Retry');
-
-    await waitForTextStatus('.pages', '4001', { timeout: 5000 });
-  });
-
-  it('should render other type', async () => {
-    await page.click('button[data-phase="set-the-other-type"]');
-    await waitForTextStatus('.njs-viz', 'The other one!');
-  });
-
   it('should destroy', async () => {
-    await page.click('button[data-phase="destroy"]');
+    const url = getScenarioUrl('destroy');
+    await page.goto(url);
     // wait for some time to ensure destroy has been run and no errors are thrown
-    await page.waitFor(100);
+    await page.waitFor(1000);
   });
 });
