f33504767fa2a6ca4e98b9713a76fbd7ccf44e2c
Thomas <thomas.bengtsson@qlik.com>
2022-01-21 11:00:44 +0100

    fix: use new specification config format

--- apis/stardust/api-spec/spec.conf.js
+++ apis/stardust/api-spec/spec.conf.js
@@ -1,42 +1,44 @@
 module.exports = {
-  glob: [
-    '!*.spec.js',
-    '../nucleus/src/**/*.js',
-    '../nucleus/src/**/*.jsx',
-    '../supernova/src/**/*.js',
-    '../locale/src/translator.js',
-    '../theme/src/**/*.js',
-    '../conversion/src/**/*.js',
-  ],
-  api: {
-    stability: 'stable',
-  },
-  output: {
-    sort: {
-      alpha: false,
+  fromJsdoc: {
+    glob: [
+      '!*.spec.js',
+      '../nucleus/src/**/*.js',
+      '../nucleus/src/**/*.jsx',
+      '../supernova/src/**/*.js',
+      '../locale/src/translator.js',
+      '../theme/src/**/*.js',
+      '../conversion/src/**/*.js',
+    ],
+    api: {
+      stability: 'stable',
     },
-    file: './api-spec/spec.json',
-  },
-  parse: {
-    types: {
-      undefined: {},
-      'qae.NxAppLayout': {},
-      'enigma.GenericObject': {},
-      'enigma.Global': {},
-      'enigma.Doc': {
-        url: 'https://github.com/qlik-oss/enigma.js/blob/master/docs/api.md#generated-api',
+    output: {
+      sort: {
+        alpha: false,
       },
-      'qae.GenericObjectLayout': {
-        url: 'https://core.qlik.com/services/qix-engine/apis/qix/definitions/#genericobjectlayout',
-      },
-      'qae.GenericObjectProperties': {
-        url: 'https://core.qlik.com/services/qix-engine/apis/qix/definitions/#genericobjectproperties',
-      },
-      'qae.NxDimension': {
-        url: 'https://core.qlik.com/services/qix-engine/apis/qix/definitions/#nxdimension',
-      },
-      'qae.NxMeasure': {
-        url: 'https://core.qlik.com/services/qix-engine/apis/qix/definitions/#nxmeasure',
+      file: './api-spec/spec.json',
+    },
+    parse: {
+      types: {
+        undefined: {},
+        'qae.NxAppLayout': {},
+        'enigma.GenericObject': {},
+        'enigma.Global': {},
+        'enigma.Doc': {
+          url: 'https://github.com/qlik-oss/enigma.js/blob/master/docs/api.md#generated-api',
+        },
+        'qae.GenericObjectLayout': {
+          url: 'https://core.qlik.com/services/qix-engine/apis/qix/definitions/#genericobjectlayout',
+        },
+        'qae.GenericObjectProperties': {
+          url: 'https://core.qlik.com/services/qix-engine/apis/qix/definitions/#genericobjectproperties',
+        },
+        'qae.NxDimension': {
+          url: 'https://core.qlik.com/services/qix-engine/apis/qix/definitions/#nxdimension',
+        },
+        'qae.NxMeasure': {
+          url: 'https://core.qlik.com/services/qix-engine/apis/qix/definitions/#nxmeasure',
+        },
       },
     },
   },
