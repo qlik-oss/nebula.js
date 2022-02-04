7166006521c3bc741c83f20117dfe55428d84e50
Johan Lahti <johanlahti@gmail.com>
2022-02-04 14:35:43 +0100

    test: use act instead of render

--- apis/nucleus/src/components/listbox/__tests__/use-selections-interactions.spec.jsx
+++ apis/nucleus/src/components/listbox/__tests__/use-selections-interactions.spec.jsx
@@ -99,21 +99,22 @@ describe('use-listbox-interactions', () => {
 
       expect(listboxSelections.selectValues).not.called;
 
-      arg0.interactionEvents.onMouseDown({
-        currentTarget: {
-          getAttribute: sandbox.stub().withArgs('data-n').returns(23),
-        },
+      act(() => {
+        arg0.interactionEvents.onMouseDown({
+          currentTarget: {
+            getAttribute: sandbox.stub().withArgs('data-n').returns(23),
+          },
+        });
       });
-      await render();
 
       const arg1 = ref.current.result;
 
       expect(listboxSelections.selectValues, 'since mouseup has not been called yet').not.called;
       expect(arg1.instantPages).to.deep.equal([]);
 
-      docMouseUpListener(); // trigger doc mouseup listener to set mouseDown => false
-
-      await render();
+      act(() => {
+        docMouseUpListener(); // trigger doc mouseup listener to set mouseDown => false
+      });
 
       const arg2 = ref.current.result;
 
@@ -131,20 +132,24 @@ describe('use-listbox-interactions', () => {
       getSelectedValues.returns([24]); // mock element nbr 24 as already selected
       await render();
       const arg0 = ref.current.result;
-      arg0.interactionEvents.onMouseDown({
-        currentTarget: {
-          getAttribute: sandbox.stub().withArgs('data-n').returns(24), // fake mousedown on element nbr 24
-        },
+
+      act(() => {
+        arg0.interactionEvents.onMouseDown({
+          currentTarget: {
+            getAttribute: sandbox.stub().withArgs('data-n').returns(24), // fake mousedown on element nbr 24
+          },
+        });
       });
-      await render();
 
       expect(applySelectionsOnPages).calledOnce;
       expect(applySelectionsOnPages.args[0]).to.deep.equal([[], [24]]);
       expect(listboxSelections.selectValues, 'should only preselect - not select - while mousedown').not.called;
       const [, docMouseUpListener] = global.document.addEventListener.args[0];
-      docMouseUpListener();
 
-      await render();
+      act(() => {
+        docMouseUpListener();
+      });
+
       expect(listboxSelections.selectValues).calledOnce.calledWithExactly({
         selections: { key: 'selections' },
         elemNumbers: [24],
@@ -165,7 +170,7 @@ describe('use-listbox-interactions', () => {
     });
 
     it('should select a range (in theory)', async () => {
-      getElemNumbersFromPages.returns([24, 25, 26, 27, 28, 29, 30, 31]); // the values which helps to fill/interpolate the range
+      getElemNumbersFromPages.returns([24, 25, 26, 27, 28, 29, 30, 31]);
 
       await render({ rangeSelect: true });
 
@@ -195,7 +200,6 @@ describe('use-listbox-interactions', () => {
           },
         });
       });
-      // act(() => {});
       act(() => {
         ref.current.result.interactionEvents.onMouseUp({
           currentTarget: {
@@ -211,7 +215,7 @@ describe('use-listbox-interactions', () => {
       expect(applySelectionsOnPages.callCount, 'should pre-select once for each new value').to.equal(4);
       expect(listboxSelections.selectValues).calledOnce.calledWithExactly({
         selections: { key: 'selections' },
-        elemNumbers: [24, 25, 28, 30],
+        elemNumbers: [24, 25, 28, 30], // without mocking fillRange this range would be filled
         isSingleSelect: false,
       });
 
