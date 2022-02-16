d086e78a090adb22c60e508a85cf8a7162f8612f
Johan Lahti <johanlahti@gmail.com>
2022-02-16 09:57:11 +0100

    fix: keep range select on

--- apis/nucleus/src/components/listbox/ListBox.jsx
+++ apis/nucleus/src/components/listbox/ListBox.jsx
@@ -18,7 +18,7 @@ export default function ListBox({
   height,
   width,
   listLayout = 'vertical',
-  rangeSelect = false,
+  rangeSelect = true,
   checkboxes = false,
   update = undefined,
 }) {
