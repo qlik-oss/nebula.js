fdc232ec8df28e25d02c0bce8ab1142433ac8c85
Thomas <thomas.bengtsson@qlik.com>
2022-02-23 11:34:16 +0100

    fix: revert typography elements to be span

--- apis/nucleus/src/components/listbox/ListBoxRowColumn.jsx
+++ apis/nucleus/src/components/listbox/ListBoxRowColumn.jsx
@@ -151,7 +151,7 @@ export default function RowColumn({ index, style, data, column = false }) {
 
   const getValueField = ({ lbl, ix, color, highlighted = false }) => (
     <Typography
-      component="div"
+      component="span"
       variant="body2"
       key={ix}
       className={[classes.labelText, highlighted && classes.highlighted, dense && classes.labelDense]
