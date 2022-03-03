bf3d3d7287d4a99542cd597c2c9f14f7f9d9c9c7
Johan Lahti <johanlahti@gmail.com>
2022-03-03 10:25:45 +0100

    fix: move styles to fieldset

--- apis/nucleus/src/components/listbox/ListBoxSearch.jsx
+++ apis/nucleus/src/components/listbox/ListBoxSearch.jsx
@@ -8,15 +8,15 @@ import InstanceContext from '../../contexts/InstanceContext';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    border: `1px solid ${theme.palette.divider}`,
-    borderWidth: '0 0 1px 0',
+    border: 'none',
     borderRadius: 0,
     '& fieldset': {
+      border: `1px solid ${theme.palette.divider}`,
+      borderWidth: '0 0 1px 0',
       borderRadius: 0,
-      border: 'none',
     },
     '&:hover': {
-      borderWidth: '0 0 1px 0',
+      border: 'none',
     },
   },
   dense: {
