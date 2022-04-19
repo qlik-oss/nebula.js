a79841d3f00dc15ec3284576bf43527bf229cc45
caele <tsm@qlik.com>
2022-04-19 14:10:02 +0200

    feat: allow data insert in mocker

--- apis/enigma-mocker/src/mocks/create-session-object-mock.js
+++ apis/enigma-mocker/src/mocks/create-session-object-mock.js
@@ -1,12 +1,21 @@
+/* eslint-disable no-underscore-dangle */
+import extend from 'extend';
+
 function CreateSessionObjectMock() {
-  return (props) =>
-    Promise.resolve({
+  return (props) => {
+    const mockedInclusions = props._mock;
+    delete props._mock; // eslint-disable-line no-param-reassign
+    const layout = mockedInclusions ? extend({}, props, mockedInclusions) : props;
+    return Promise.resolve({
       on: () => {},
       once: () => {},
-      getLayout: () => Promise.resolve({}),
+      getLayout: () => Promise.resolve(layout),
+      getProperties: () => Promise.resolve(props),
+      getEffectiveProperties: () => Promise.resolve(props),
       id: props && props.qInfo && props.qInfo.qId ? props.qInfo.qId : `sel - ${+Date.now()}`,
       ...props,
     });
+  };
 }
 
 export default CreateSessionObjectMock;
