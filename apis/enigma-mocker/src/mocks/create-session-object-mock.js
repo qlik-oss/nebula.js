3b91ddd701f72b0b73b5d70938b62d5ad8863cba
caele <tsm@qlik.com>
2022-05-11 10:46:38 +0200

    chore: change id generation

--- apis/enigma-mocker/src/mocks/create-session-object-mock.js
+++ apis/enigma-mocker/src/mocks/create-session-object-mock.js
@@ -1,10 +1,26 @@
 /* eslint-disable no-underscore-dangle */
 import extend from 'extend';
 
+const crt = global.crypto || { getRandomValues: () => 123456 };
+
+// https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+// Not using crypto.randomUUID due to missing safari support < 15
+function uuidv4() {
+  // To cover test
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (c ^ (crt.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+  );
+}
+
 function CreateSessionObjectMock() {
   return (props) => {
-    const mockedInclusions = props ? props._mock : null;
+    props = props || {}; // eslint-disable-line no-param-reassign
+    props.qInfo = props.qInfo || {}; // eslint-disable-line no-param-reassign
+    props.qInfo.qId = props.qInfo.qId || `mock-${uuidv4()}`; // eslint-disable-line no-param-reassign
+
+    const mockedInclusions = props._mock;
     let layout = props;
+
     if (mockedInclusions) {
       delete props._mock; // eslint-disable-line no-param-reassign
       layout = extend({}, props, mockedInclusions);
@@ -15,7 +31,7 @@ function CreateSessionObjectMock() {
       getLayout: () => Promise.resolve(layout),
       getProperties: () => Promise.resolve(props),
       getEffectiveProperties: () => Promise.resolve(props),
-      id: props && props.qInfo && props.qInfo.qId ? props.qInfo.qId : `sel - ${+Date.now()}`,
+      id: props.qInfo.qId,
       ...props,
     });
   };
