90ec1fa544a75d69e891f5b63671fcff2b54fdfa
Bradley Gyemi <htx@qlik.com>
2022-05-17 14:45:38 +0200

    fix: using sh to call sn generation script

--- commands/serve/native/scripts/setEmptySn.sh
+++ commands/serve/native/scripts/setEmptySn.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "here is arg 0"
+echo $0
+
 basedir=$(dirname $0)
-echo "const sn = () => {return (<></>);}\nexport default sn" > ./src/sn/sn.js
+
+echo "const sn = () => {return (<></>);}\n\nexport default sn" > $basedir/../src/sn/sn.js
