631c3ec15468a34d5a6d5bb88542b93bd5fa8d21
Bradley Gyemi <htx@qlik.com>
2022-05-19 14:00:10 +0200

    fix: fix lint error

--- commands/serve/native/scripts/setEmptySn.sh
+++ commands/serve/native/scripts/setEmptySn.sh
@@ -8,4 +8,4 @@ basedir=$(dirname $0)
 echo "here is basedir"
 echo $basedir
 
-echo "import React from 'react'; \n\nconst sn = () => return <></>;\n\nexport default sn" > $basedir/../src/sn/sn.js
+echo "/* eslint-disable */ \n\nimport React from 'react'; \n\nconst sn = () => return <></>;\n\nexport default sn" > $basedir/../src/sn/sn.js
