ff1aa8ad70c1a2f75a46f8fa3966499805bf078b
Li Kang <kkllpaul7766@gmail.com>
2022-05-30 19:06:27 +0200

    docs(contributing): add packages and development worlflow (#836)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = ({ config }) => {
-  const jsRule = config.module.rules.find(rule => rule.test.test('.js'));
+  const jsRule = config.module.rules.find((rule) => rule.test.test('.js'));
   jsRule.exclude = /node_modules/;
 
   return config;
