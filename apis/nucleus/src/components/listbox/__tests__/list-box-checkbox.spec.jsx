edb8a4d28eb8af6b74ce9c736f5c377d5f68880d
caele <tsm@qlik.com>
2022-06-02 08:49:35 +0200

    fix: listbox tests

--- apis/nucleus/src/components/listbox/__tests__/list-box-checkbox.spec.jsx
+++ apis/nucleus/src/components/listbox/__tests__/list-box-checkbox.spec.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Checkbox } from '@mui/material';
+import { createTheme, ThemeProvider } from '@nebula.js/ui/theme';
 import ListBoxCheckbox from '../ListBoxCheckbox';
 
 async function render(content) {
@@ -12,8 +13,11 @@ async function render(content) {
 }
 
 describe('<ListBoxCheckbox />', () => {
+  const theme = createTheme('dark');
+  const getRenderSetup = (component) => <ThemeProvider theme={theme}>{component}</ThemeProvider>;
+
   it('should render an unchecked checkbox', async () => {
-    const testRenderer = await render(<ListBoxCheckbox label="just check it" />);
+    const testRenderer = await render(getRenderSetup(<ListBoxCheckbox label="just check it" />));
     const cbs = testRenderer.root.findAllByType(Checkbox);
     expect(cbs).to.have.length(1);
     const [cb] = cbs;
@@ -28,7 +32,7 @@ describe('<ListBoxCheckbox />', () => {
   });
 
   it('should render a checked checkbox', async () => {
-    const testRenderer = await render(<ListBoxCheckbox checked label="just check it" />);
+    const testRenderer = await render(getRenderSetup(<ListBoxCheckbox checked label="just check it" />));
     const cbs = testRenderer.root.findAllByType(Checkbox);
     expect(cbs).to.have.length(1);
     const [cb] = cbs;
@@ -37,28 +41,32 @@ describe('<ListBoxCheckbox />', () => {
   });
 
   it('should render checkbox filled with alternative gray', async () => {
-    const testRenderer = await render(<ListBoxCheckbox alternative label="filled with gray" />);
+    const testRenderer = await render(getRenderSetup(<ListBoxCheckbox alternative label="filled with gray" />));
     const cb = testRenderer.root.findByType(Checkbox);
     expect(cb.props.className).to.equal('checkbox');
     expect(cb.props.icon.props.children.props.className).to.equal('cbIconAlternative');
   });
 
   it('should not render checkbox filled with alternative gray when showGray is false', async () => {
-    const testRenderer = await render(<ListBoxCheckbox alternative showGray={false} label="filled with gray" />);
+    const testRenderer = await render(
+      getRenderSetup(<ListBoxCheckbox alternative showGray={false} label="filled with gray" />)
+    );
     const cb = testRenderer.root.findByType(Checkbox);
     expect(cb.props.className).to.equal('checkbox');
     expect(cb.props.icon.props.children.props.className).to.equal('');
   });
 
   it('should render checkbox filled with excluded gray', async () => {
-    const testRenderer = await render(<ListBoxCheckbox excluded label="filled with gray" />);
+    const testRenderer = await render(getRenderSetup(<ListBoxCheckbox excluded label="filled with gray" />));
     const cb = testRenderer.root.findByType(Checkbox);
     expect(cb.props.className).to.equal('checkbox');
     expect(cb.props.icon.props.children.props.className).to.equal('cbIconExcluded');
   });
 
   it('should not render checkbox filled with excluded gray when showGray is false', async () => {
-    const testRenderer = await render(<ListBoxCheckbox excluded showGray={false} label="filled with gray" />);
+    const testRenderer = await render(
+      getRenderSetup(<ListBoxCheckbox excluded showGray={false} label="filled with gray" />)
+    );
     const cb = testRenderer.root.findByType(Checkbox);
     expect(cb.props.className).to.equal('checkbox');
     expect(cb.props.icon.props.children.props.className).to.equal('');
--- apis/nucleus/src/components/listbox/__tests__/list-box-inline.spec.jsx
+++ apis/nucleus/src/components/listbox/__tests__/list-box-inline.spec.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { create, act } from 'react-test-renderer';
 import { IconButton, Grid, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@nebula.js/ui/theme';
 
 describe('<ListboxInline />', () => {
   let sandbox;
@@ -22,8 +23,6 @@ describe('<ListboxInline />', () => {
   let ActionsToolbar;
   let ListBoxSearch;
   let createListboxSelectionToolbar;
-  let useTheme;
-  let theme;
   let layout;
   let useSessionModel;
   let useObjectSelections;
@@ -54,11 +53,6 @@ describe('<ListboxInline />', () => {
     getListboxInlineKeyboardNavigation = sandbox.stub().returns('keyboard-navigation');
     ListBoxSearch = sandbox.stub();
     createListboxSelectionToolbar = sandbox.stub();
-    useTheme = sandbox.stub();
-    theme = {
-      spacing: sandbox.stub(),
-      palette: { divider: 'red' },
-    };
     layout = {
       title: 'title',
 
@@ -136,10 +130,8 @@ describe('<ListboxInline />', () => {
       fetchStart: 'fetchStart',
     };
 
-    theme.spacing.returns('padding');
     useState.callsFake((startValue) => [startValue, () => {}]);
     useRef.returns({ current: 'current' });
-    useTheme.returns(theme);
     createListboxSelectionToolbar.returns('actions');
 
     ActionsToolbar.returns('ActionsToolbar');
@@ -181,12 +173,16 @@ describe('<ListboxInline />', () => {
 
   describe('Check rendering with different options', () => {
     beforeEach(() => {
+      const theme = createTheme('dark');
+
       render = async () => {
         await act(async () => {
           renderer = create(
-            <InstanceContext.Provider value={{ translator: { get: (s) => s, language: () => 'sv' } }}>
-              <ListBoxInline app={app} fieldIdentifier={fieldIdentifier} stateName={stateName} options={options} />
-            </InstanceContext.Provider>
+            <ThemeProvider theme={theme}>
+              <InstanceContext.Provider value={{ translator: { get: (s) => s, language: () => 'sv' } }}>
+                <ListBoxInline app={app} fieldIdentifier={fieldIdentifier} stateName={stateName} options={options} />
+              </InstanceContext.Provider>
+            </ThemeProvider>
           );
         });
       };
