af93cde6064a5415d6f88051711f61d7eb87c0d6
Tobias Linsefors <tobias.linsefors@qlik.com>
2022-06-09 11:32:26 +0200

    fix: fix OneAndOnlyOne in app and object selection toolbar

--- apis/nucleus/src/components/selections/OneField.jsx
+++ apis/nucleus/src/components/selections/OneField.jsx
@@ -91,26 +91,31 @@ export default function OneField({
       </Grid>
     );
 
-    Icon = selection.qLocked ? (
-      <Grid item>
-        <IconButton size="large">
-          <Lock />
-        </IconButton>
-      </Grid>
-    ) : (
-      <Grid item>
-        <IconButton
-          title={translator.get('Selection.Clear')}
-          onClick={(e) => {
-            e.stopPropagation();
-            api.clearField(selection.qField, field.states[stateIx]);
-          }}
-          size="large"
-        >
-          <Remove />
-        </IconButton>
-      </Grid>
-    );
+    if (selection.qLocked) {
+      Icon = (
+        <Grid item>
+          <IconButton size="large">
+            <Lock />
+          </IconButton>
+        </Grid>
+      );
+    } else if (!selection.qOneAndOnlyOne) {
+      Icon = (
+        <Grid item>
+          <IconButton
+            title={translator.get('Selection.Clear')}
+            onClick={(e) => {
+              e.stopPropagation();
+              api.clearField(selection.qField, field.states[stateIx]);
+            }}
+            size="large"
+          >
+            <Remove />
+          </IconButton>
+        </Grid>
+      );
+    }
+
     SegmentsIndicator = (
       <div
         style={{
