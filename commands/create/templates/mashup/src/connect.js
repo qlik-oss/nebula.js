44e8bd6d480cc2ac8eee6d859dd5797d7aeecd9d
a-m-dev <a.mirzaei.dev@gmail.com>
2022-09-08 09:58:53 +0200

    feat: integrate `@qlik/sdk` into mashup template

--- commands/create/templates/mashup/src/connect.js
+++ commands/create/templates/mashup/src/connect.js
@@ -1,44 +1,24 @@
 import enigma from 'enigma.js';
 import schema from 'enigma.js/schemas/12.936.0.json';
-
-async function getQCSHeaders({ webIntegrationId, url }) {
-  const response = await fetch(`${url}/api/v1/csrf-token`, {
-    credentials: 'include',
-    headers: { 'qlik-web-integration-id': webIntegrationId },
-  });
-  if (response.status === 401) {
-    const loginUrl = new URL(`${url}/login`);
-    loginUrl.searchParams.append('returnto', window.location.href);
-    loginUrl.searchParams.append('qlik-web-integration-id', webIntegrationId);
-    window.location.href = loginUrl;
-    return undefined;
-  }
-  const csrfToken = new Map(response.headers).get('qlik-csrf-token');
-  return {
-    'qlik-web-integration-id': webIntegrationId,
-    'qlik-csrf-token': csrfToken,
-  };
-}
-
-async function getEnigmaApp({ host, appId, headers }) {
-  const params = Object.keys(headers)
-    .map((key) => `${key}=${headers[key]}`)
-    .join('&');
-
-  const enigmaGlobal = await enigma
-    .create({
-      schema,
-      url: `wss://${host}/app/${appId}?${params}`,
-    })
-    .open();
-
-  return enigmaGlobal.openDoc(appId);
-}
+import { Auth, AuthType } from '@qlik/sdk';
 
 async function connect({ url, webIntegrationId, appId }) {
-  const host = url.replace(/^https?:\/\//, '').replace(/\/?/, '');
-  const headers = await getQCSHeaders({ url, webIntegrationId });
-  return getEnigmaApp({ host, headers, appId });
+  const authInstance = new Auth({
+    webIntegrationId,
+    autoRedirect: true,
+    authType: AuthType.WebIntegration,
+    host: url.replace(/^https?:\/\//, '').replace(/\/?/, ''),
+  });
+
+  if (!authInstance.isAuthenticated()) {
+    authInstance.authenticate();
+  } else {
+    const wssUrl = await authInstance.generateWebsocketUrl(appId);
+    const enigmaGlobal = await enigma.create({ schema, url: wssUrl }).open();
+    return enigmaGlobal.openDoc(appId);
+  }
+
+  return null;
 }
 
 export default connect;
