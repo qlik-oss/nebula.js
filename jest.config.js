32fb23c9a1b7feab9a48d79b45d59b335e5b6c62
a-m-dev <a.mirzaei.dev@gmail.com>
2022-09-28 18:30:52 +0200

    test: `apis/enigma-mocker` folder covered

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   clearMocks: true,
   testEnvironment: 'jest-environment-jsdom',
-  testRegex: ['commands/.+\\.(spec|test|inspect)\\.[jt]sx?$', 'apis/conversion/.+\\.inspect\\.[jt]sx?$'],
+  testRegex: [
+    'commands/.+\\.(spec|test|inspect)\\.[jt]sx?$',
+    'apis/conversion/.+\\.inspect\\.[jt]sx?$',
+    'apis/enigma-mocker/.+\\.inspect\\.[jt]sx?$',
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   collectCoverageFrom: [
     'commands/**/*.{js,jsx}',
     'apis/conversion/**/*.{js,jsx}',
+    'apis/enigma-mocker/**/*.{js,jsx}',
+    '!apis/enigma-mocker/examples/**',
     '!**/lib/**',
     '!**/dist/**',
     '!**/node_modules/**',
