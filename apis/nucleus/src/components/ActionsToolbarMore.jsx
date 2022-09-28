3660962818fafb666154545348c99a18c7aa05eb
CaseyADASC <Casey@adasitecompliance.com>
2022-09-28 14:02:52 -0400

    fix(a360 451): add aria-controls & aria-expanded to more menu button

--- apis/nucleus/src/components/ActionsToolbarMore.jsx
+++ apis/nucleus/src/components/ActionsToolbarMore.jsx
@@ -73,7 +73,7 @@ const More = React.forwardRef(
             },
           }}
         >
-          <MenuList>
+          <MenuList id="moreMenuList">
             {actions.map((item, ix) => (
               // eslint-disable-next-line react/no-array-index-key
               <MoreItem key={ix} item={item} onActionClick={onCloseOrActionClick} />
