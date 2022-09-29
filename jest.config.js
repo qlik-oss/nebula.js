93eafbde816581cbe1990fb28077c99b9cff1525
a-m-dev <a.mirzaei.dev@gmail.com>
2022-09-29 12:45:38 +0200

    test: `qae.js` module covered

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     'apis/enigma-mocker/.+\\.inspect\\.[jt]sx?$',
     'apis/locale/.+\\.inspect\\.[jt]sx?$',
     'apis/snapshooter/.+\\.inspect\\.[jt]sx?$',
+    'apis/supernova/.+\\.inspect\\.[jt]sx?$',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   collectCoverageFrom: [
@@ -15,10 +16,18 @@ module.exports = {
     'apis/enigma-mocker/**/*.{js,jsx}',
     'apis/locale/**/*.{js,jsx}',
     'apis/snapshooter/**/*.{js,jsx}',
+    'apis/supernova/**/*.{js,jsx}',
+
     '!apis/enigma-mocker/examples/**',
+    '!apis/enigma-mocker/index.js',
+    '!commands/create/**/*.{js,jsx}',
+    '!apis/snapshooter/client.js',
+
     '!**/lib/**',
     '!**/dist/**',
     '!**/node_modules/**',
+    '!**/*.config.js',
+    '!**/*.conf.js',
   ],
   coverageReporters: ['json', 'lcov', 'text-summary', 'clover'],
 };
