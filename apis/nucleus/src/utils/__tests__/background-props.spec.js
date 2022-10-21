2f3126d7d3c49a20436f70498db4e9d4736546d5
caele <tsm@qlik.com>
2022-10-21 11:18:51 +0200

    chore: add tests files

---
+++ apis/nucleus/src/utils/__tests__/background-props.spec.js
@@ -0,0 +1,58 @@
+import theme from '@nebula.js/theme';
+import { resolveBgColor, resolveBgImage } from '../background-props';
+
+describe('Background property resolver', () => {
+  let bgCompLayout;
+
+  const t = theme().externalAPI;
+
+  const app = {
+    session: {
+      config: {
+        url: 'wss://example.com/lots/of/paths',
+      },
+    },
+  };
+
+  beforeEach(() => {
+    bgCompLayout = {
+      key: 'general',
+      bgColor: {
+        useColorExpression: true,
+        colorExpression: '#ff0000',
+        color: { index: -1, color: 'aqua' },
+      },
+      bgImage: {
+        mode: 'media',
+        mediaUrl: {
+          qStaticContentUrl: {
+            qUrl: '/media/Tulips.jpg',
+          },
+        },
+        position: 'top-left',
+        sizing: 'alwaysFit',
+      },
+    };
+  });
+
+  it('should resolve background color by expression', () => {
+    const color = resolveBgColor(bgCompLayout, t);
+    expect(color).to.equal('rgb(255,0,0)');
+  });
+  it('should resolve background color by picker', () => {
+    bgCompLayout.bgColor.useColorExpression = false;
+    const color = resolveBgColor(bgCompLayout, t);
+    expect(color).to.equal('aqua');
+  });
+  it('should resolve background image https', () => {
+    const { url, pos } = resolveBgImage(bgCompLayout, app);
+    expect(url).to.equal('https://example.com/lots/of/paths/media/Tulips.jpg');
+    expect(pos).to.equal('top left');
+  });
+  it('should resolve background image http', () => {
+    app.session.config.url = 'ws://example.com/lots/of/paths';
+    const { url, size } = resolveBgImage(bgCompLayout, app);
+    expect(url).to.equal('http://example.com/lots/of/paths/media/Tulips.jpg');
+    expect(size).to.equal('contain');
+  });
+});
---
+++ apis/nucleus/src/utils/background-props.js
@@ -0,0 +1,91 @@
+const imageSizingToCssProperty = {
+  originalSize: 'auto auto',
+  alwaysFit: 'contain',
+  fitWidth: '100% auto',
+  fitHeight: 'auto 100%',
+  stretchFit: '100% 100%',
+  alwaysFill: 'cover',
+};
+
+const positionToCss = {
+  'top-left': 'top left',
+  'top-center': 'top center',
+  'top-right': 'top right',
+  'center-left': 'center left',
+  'center-center': 'center center',
+  'center-right': 'center right',
+  'bottom-left': 'bottom left',
+  'bottom-center': 'bottom center',
+  'bottom-right': 'bottom right',
+};
+
+// TODO: this needs some proper verification
+function getSenseServerUrl(app) {
+  let config;
+  let wsUrl;
+  let protocol;
+  let isSecure;
+
+  if (app?.session?.config) {
+    config = app.session.config;
+    wsUrl = new URL(config.url);
+
+    isSecure = wsUrl.protocol === 'wss:';
+    protocol = isSecure ? 'https://' : 'http://';
+    return protocol + wsUrl.host;
+  }
+  return undefined;
+}
+
+function getBackgroundPosition(bgComp) {
+  let bkgImagePosition = 'center center';
+  if (bgComp?.bgImage?.position) {
+    bkgImagePosition = positionToCss[bgComp.bgImage.position];
+  }
+  return bkgImagePosition;
+}
+
+function getBackgroundSize(bgComp) {
+  let bkgImageSize = imageSizingToCssProperty.originalSize;
+  if (bgComp?.bgImage?.sizing) {
+    bkgImageSize = imageSizingToCssProperty[bgComp.bgImage.sizing];
+  }
+  return bkgImageSize;
+}
+
+function resolveImageUrl(app, relativeUrl) {
+  return relativeUrl ? getSenseServerUrl(app) + relativeUrl : undefined;
+}
+
+export const resolveBgImage = (bgComp, app) => {
+  const bgImageDef = bgComp?.bgImage;
+
+  if (bgImageDef) {
+    let url = '';
+    if (bgImageDef.mode === 'media') {
+      url = bgImageDef?.mediaUrl?.qStaticContentUrl?.qUrl
+        ? decodeURIComponent(bgImageDef.mediaUrl.qStaticContentUrl.qUrl)
+        : undefined;
+      url = resolveImageUrl(app, url);
+    }
+    if (bgImageDef.mode === 'expression') {
+      url = bgImageDef.expressionUrl ? decodeURIComponent(bgImageDef.expressionUrl) : undefined;
+    }
+    const pos = getBackgroundPosition(bgComp);
+    const size = getBackgroundSize(bgComp);
+
+    return url ? { url, pos, size } : undefined;
+  }
+  return undefined;
+};
+
+export const resolveBgColor = (bgComp, theme) => {
+  const bgColor = bgComp?.bgColor;
+  if (bgColor && theme) {
+    if (bgColor.useColorExpression) {
+      return theme.validateColor(bgColor.colorExpression);
+    }
+    return bgColor.color && bgColor.color.color !== 'none' ? theme.getColorPickerColor(bgColor.color) : undefined;
+  }
+  return undefined;
+};
