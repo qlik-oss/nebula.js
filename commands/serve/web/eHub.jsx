7217b180e01e3dde501266dc9ccf442778308527
a-m-dev <a.mirzaei.dev@gmail.com>
2022-10-26 14:03:50 +0200

    chore: `<HubLayout />`

--- commands/serve/web/eHub.jsx
+++ commands/serve/web/eHub.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import { Root } from './Root';
+import { Root } from './components/Root';
 
 ReactDOM.createRoot(document.querySelector('#app')).render(<Root />);
