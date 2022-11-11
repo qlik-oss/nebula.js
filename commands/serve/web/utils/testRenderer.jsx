b9b2e0d41a92edd3f96bd90a62e9eb321a3de413
a-m-dev <a.mirzaei.dev@gmail.com>
2022-11-11 12:00:36 +0100

    test: base test renderer

---
+++ commands/serve/web/utils/testRenderer.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvents from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@nebula.js/ui/theme';
+
+import { RootContext } from '../contexts/RootContext';
+
+const theme = createTheme('light');
+
+export function TestRenderer(component, contextValuesOverride) {
+  const contextValue = {
+    // context values
+    storage: () => {},
+
+    // useInfo result
+    info: {},
+    setInfo: () => {},
+
+    // useConnection result
+    glob: {},
+    setGlobal: () => {},
+    treatAsDesktop: false,
+    setTreatAsDesktop: () => {},
+    error: {},
+    setError: () => {},
+    activeStep: 0,
+    setActiveStep: () => {},
+
+    // useCachedConnections result
+    cachedConnectionsData: {
+      cachedConnections: [],
+      addCachedConnections: () => {},
+      removeCachedConnection: () => {},
+    },
+
+    // override what ever you want in render level
+    ...contextValuesOverride,
+  };
+
+  const Wrapper = ({ children }) => {
+    return (
+      <BrowserRouter>
+        <RootContext.Provider value={contextValue}>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </RootContext.Provider>
+      </BrowserRouter>
+    );
+  };
+
+  return { ...render(component, { wrapper: Wrapper }), userEvents: userEvents.setup() };
+}
