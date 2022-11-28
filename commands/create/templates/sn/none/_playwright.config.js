7631d7463db8449ab3293426f5caa1432a349366
a-m-dev <a.mirzaei.dev@gmail.com>
2022-11-28 14:28:06 +0100

    chore: headless to `true`

--- commands/create/templates/sn/none/_playwright.config.js
+++ commands/create/templates/sn/none/_playwright.config.js
@@ -17,7 +17,7 @@ export default {
   ],
 
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     screenshot: 'only-on-failure',
