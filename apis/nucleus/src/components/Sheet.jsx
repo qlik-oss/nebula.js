3b767ac56ef80e994453103417086a3539521a96
caele <tsm@qlik.com>
2022-12-07 09:31:07 +0100

    chore: use memo

--- apis/nucleus/src/components/Sheet.jsx
+++ apis/nucleus/src/components/Sheet.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useMemo } from 'react';
 import useLayout from '../hooks/useLayout';
 import getObject from '../object/get-object';
 import Cell from './Cell';
@@ -31,30 +31,55 @@ function Sheet({ model, halo, initialSnOptions, initialSnPlugins, initialError, 
   const [cells, setCells] = useState([]);
   const [bgColor, setBgColor] = useState(undefined);
   const [bgImage, setBgImage] = useState(undefined);
+  const [deepHash, setDeepHash] = useState('');
 
   /// For each object
   useEffect(() => {
     if (layout) {
+      const hash = JSON.stringify(layout.cells);
+      if (hash === deepHash) {
+        return;
+      }
+      setDeepHash(hash);
       const fetchObjects = async () => {
+        /*
+          Need to always fetch and evaluate everything as the sheet need to support multiple instances of the same object?
+          No, there is no way to add the same chart twice, so the optimization should be worth it.
+        */
+
+        // Clear the cell list
+        cells.forEach((c) => {
+          root.removeCell(c.currentId);
+        });
+
         const lCells = layout.cells;
-        const vs = await Promise.all(lCells.map((c) => getObject({ id: c.name }, halo)));
         // TODO - should try reuse existing objects on subsequent renders
         // Non-id updates should only change the "css"
-        const cs = lCells.map((c, i) => {
-          let mounted;
-          const mountedPromise = new Promise((resolve) => {
-            mounted = resolve;
-          });
-          return {
-            model: vs[i].model,
-            id: c.name,
-            bounds: c.bounds,
-            cellRef: React.createRef(),
-            currentId: uid(),
-            mounted,
-            mountedPromise,
-          };
-        });
+        const cs = await Promise.all(
+          lCells.map(async (c, i) => {
+            let mounted;
+            const mountedPromise = new Promise((resolve) => {
+              mounted = resolve;
+            });
+
+            const cell = cells.find((ce) => ce.id === c.name);
+            if (cell) {
+              cell.bounds = c.bounds;
+              delete cell.mountedPromise;
+              return cell;
+            }
+            const vs = await getObject({ id: c.name }, halo);
+            return {
+              model: vs.model,
+              id: c.name,
+              bounds: c.bounds,
+              cellRef: React.createRef(),
+              currentId: uid(),
+              mounted,
+              mountedPromise,
+            };
+          })
+        );
         cs.forEach((c) => {
           // TODO - these needs to be removed as well
           root.addCell(c.currentId, c.cellRef);
@@ -64,6 +89,12 @@ function Sheet({ model, halo, initialSnOptions, initialSnPlugins, initialError, 
       fetchObjects();
     }
   }, [layout]);
+
+  const cellRenderers = useMemo(() => {
+    return cells
+      ? cells.map((c) => getCellRenderer(c, halo, initialSnOptions, initialSnPlugins, initialError, c.mounted))
+      : [];
+  }, [cells]);
 
   useEffect(() => {
     const bgComp = layout?.components ? layout.components.find((comp) => comp.key === 'general') : null;
@@ -76,32 +107,31 @@ function Sheet({ model, halo, initialSnOptions, initialSnPlugins, initialError, 
     - sheet exposed classnames for theming
   */
 
-  if (cells && cells.length) {
-    const height = Number.isNaN(layout.height) ? '100%' : `${Number(layout.height)}%`;
-    const promises = cells.map((c) => c.mountedPromise);
+  const height = !layout || Number.isNaN(layout.height) ? '100%' : `${Number(layout.height)}%`;
+  const promises = cells.map((c) => c.mountedPromise);
+  const ps = promises.filter((p) => !!p);
+  if (ps.length) {
     Promise.all(promises).then(() => {
-      // TODO - this should only be called once, when all the initial cells has rendered
+      // TODO - correct? Currently called each time a new cell is mounted?
       onMount();
     });
-    return (
-      <div
-        style={{
-          width: `100%`,
-          height,
-          position: 'relative',
-          backgroundColor: bgColor,
-          backgroundImage: bgImage && bgImage.url ? `url(${bgImage.url})` : undefined,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: bgImage && bgImage.size,
-          backgroundPosition: bgImage && bgImage.pos,
-        }}
-      >
-        {cells.map((c) => getCellRenderer(c, halo, initialSnOptions, initialSnPlugins, initialError, c.mounted))}
-      </div>
-    );
   }
-
-  return layout && layout.qMeta && layout.qMeta.title ? <div>{layout.qMeta.title}</div> : null;
+  return (
+    <div
+      style={{
+        width: `100%`,
+        height,
+        position: 'relative',
+        backgroundColor: bgColor,
+        backgroundImage: bgImage && bgImage.url ? `url(${bgImage.url})` : undefined,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: bgImage && bgImage.size,
+        backgroundPosition: bgImage && bgImage.pos,
+      }}
+    >
+      {cellRenderers}
+    </div>
+  );
 }
 
 export default Sheet;
