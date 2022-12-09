57c402629c69f417dbb4f3640212afbacf4ce0e0
caele <tsm@qlik.com>
2022-12-09 13:47:26 +0100

    chore: settings

--- scripts/run-rendering-tests.js
+++ scripts/run-rendering-tests.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
 
-const cmd = 'mocha test/rendering/**/*.spec.js --bail false --timeout 30000';
+const cmd =
+  'mocha test/rendering/**/*.spec.js --bail false --timeout 30000  --chrome.browserWSEndpoint "ws://localhost:3000" --no-launch';
 execSync(cmd, { stdio: 'inherit' });
