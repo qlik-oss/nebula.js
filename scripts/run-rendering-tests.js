f9d21e9367626f07c1235a02df578a6e23a72c20
caele <tsm@qlik.com>
2022-12-12 13:34:34 +0100

    chore: revert test tests

--- scripts/run-rendering-tests.js
+++ scripts/run-rendering-tests.js
@@ -1,5 +1,4 @@
 const { execSync } = require('child_process');
 
-const cmd =
-  'mocha test/rendering/**/*.spec.js --bail false --timeout 30000  --chrome.browserWSEndpoint "ws://localhost:3000" --no-launch';
+const cmd = 'mocha test/rendering/**/*.spec.js --bail false --timeout 30000';
 execSync(cmd, { stdio: 'inherit' });
