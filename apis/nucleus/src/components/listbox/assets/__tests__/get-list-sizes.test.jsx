5f2a71ed792466938d93509aafab03b75d81a414
Johan Lahti <johanlahti@gmail.com>
2023-01-05 10:20:21 +0100

    test: adapt

--- apis/nucleus/src/components/listbox/assets/__tests__/get-list-sizes.test.jsx
+++ apis/nucleus/src/components/listbox/assets/__tests__/get-list-sizes.test.jsx
@@ -7,6 +7,7 @@ describe('get-list-sizes', () => {
     args = {
       layout: {
         layoutOptions: {
+          dataLayout: 'singleColumn',
           layoutOrder: 'row',
           maxVisibleRows: {
             maxRows: 3,
@@ -65,29 +66,38 @@ describe('get-list-sizes', () => {
     expect(sizes).toMatchObject({ itemSize: 20 });
   });
 
-  it('layoutOrder column', () => {
-    args.layoutOrder = 'column';
+  it('grid mode with layoutOrder column', () => {
+    args.layout.layoutOptions.dataLayout = 'grid';
+    args.layout.layoutOptions.layoutOrder = 'column';
     const sizes = getListSizes(args);
     expect(sizes).toEqual({
-      columnCount: 4,
-      columnWidth: 47.5,
+      columnCount: 17,
+      columnWidth: 68,
       count: 200,
-      itemSize: 33,
+      itemSize: 45,
       listCount: 100,
       listHeight: 300,
       overflowStyling: {
-        overflowX: 'hidden',
+        overflowY: 'hidden',
       },
-      rowCount: 25,
+      rowCount: 6,
       scrollBarWidth: 10,
     });
   });
 
   it('layoutOrder column with auto visible columns mode should return a different rowCount and columnCount', () => {
+    args.layout.layoutOptions.dataLayout = 'grid';
     args.layout.layoutOptions.maxVisibleColumns.auto = true;
-    args.layoutOrder = 'column';
+    args.layout.layoutOptions.layoutOrder = 'column';
     args.maxRows = 2;
     const sizes = getListSizes(args);
-    expect(sizes).toMatchObject({ rowCount: 34, columnCount: 3 });
+    expect(sizes).toMatchObject({ rowCount: 6, columnCount: 17 });
+  });
+
+  it('grid mode with layoutOrder == column should add exta 12px padding to the itemSize', () => {
+    args.layout.layoutOptions.dataLayout = 'grid';
+    args.layout.layoutOptions.layoutOrder = 'column';
+    const sizes = getListSizes(args);
+    expect(sizes.itemSize).toEqual(45); // itemSize + padding = 33 + 12 = 45
   });
 });
