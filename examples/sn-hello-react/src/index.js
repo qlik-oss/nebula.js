eb486e2e5a42b6be341bab56f27babf4a2ce18f9
caele <tsm@qlik.com>
2023-01-18 09:23:44 +0100

    chore: update templates to use inline ext

--- examples/sn-hello-react/src/index.js
+++ examples/sn-hello-react/src/index.js
@@ -2,15 +2,24 @@ import { useElement, useLayout, useEffect } from '@nebula.js/stardust';
 
 import properties from './object-properties';
 import data from './data';
+import ext from './ext';
 
 import { render, teardown } from './components/root';
-
-export default function supernova(/* env */) {
+/**
+ * Entrypoint for your sense visualization
+ * @param {object} galaxy Contains global settings from the environment.
+ * Useful for cases when stardust hooks are unavailable (ie: outside the component function)
+ * @param {object} galaxy.anything Extra environment dependent options
+ * @param {object=} galaxy.anything.sense Optional object only present within Sense,
+ * see: https://qlik.dev/libraries-and-tools/nebulajs/in-qlik-sense
+ */
+export default function supernova(galaxy) {
   return {
     qae: {
       properties,
       data,
     },
+    ext: ext(galaxy),
     component() {
       const el = useElement();
       const layout = useLayout();
