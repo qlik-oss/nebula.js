63a71da09dd0b9c73b1125cc4ebb940cd7d720aa
caele <tsm@qlik.com>
2023-03-03 16:23:31 +0100

    chore: update nebulaapp tests again

--- apis/nucleus/src/components/__tests__/nebulaapp.test.jsx
+++ apis/nucleus/src/components/__tests__/nebulaapp.test.jsx
@@ -2,29 +2,26 @@
 /* eslint-disable no-import-assign */
 import React from 'react';
 import { create, act } from 'react-test-renderer';
-import * as reactDomModule from 'react-dom';
 import { ThemeProvider, StyledEngineProvider, createTheme } from '@nebula.js/ui/theme';
 import * as InstanceContextModule from '../../contexts/InstanceContext';
 import * as useAppSelectionsModule from '../../hooks/useAppSelections';
 
 import boot, { NebulaApp } from '../NebulaApp';
 
-const mockedRoot = { render: jest.fn() };
-jest.mock('react-dom', () => ({
+let mockedRoot;
+jest.mock('react-dom/client', () => ({
   createRoot: () => mockedRoot,
 }));
 
 describe('Boot NebulaApp', () => {
   let InstanceContext;
-  let renderMock;
 
   beforeEach(() => {
-    renderMock = jest.fn();
+    mockedRoot = { render: jest.fn() };
 
     InstanceContext = React.createContext();
     InstanceContextModule.default = InstanceContext;
     jest.spyOn(useAppSelectionsModule, 'default').mockImplementation(() => [{}]);
-    jest.spyOn(reactDomModule, 'render').mockImplementation(renderMock);
 
     const mockedElement = {
       style: {},
@@ -43,8 +40,9 @@ describe('Boot NebulaApp', () => {
 
   test('should call ReactDOM render', () => {
     boot({ app: { id: 'foo' } });
-    expect(renderMock).toHaveBeenCalledTimes(1);
+    expect(mockedRoot.render).toHaveBeenCalledTimes(1);
   });
+
   test('should return api', () => {
     const [api] = boot({ app: { id: 'foo' } });
     expect(api.add instanceof Function).toBe(true);
@@ -54,92 +52,67 @@ describe('Boot NebulaApp', () => {
   });
 
   describe('test api methods', () => {
-    let renderCallback;
-
-    beforeEach(() => {
-      jest.spyOn(reactDomModule, 'render').mockImplementation((el, container, callback) => {
-        renderCallback = callback;
-      });
-    });
-
-    afterEach(() => {
-      jest.restoreAllMocks();
-      jest.resetAllMocks();
-    });
-
     test('should add component', async () => {
       const app = { id: 'foo' };
       const translator = {};
-      const [api, appRef, rendered] = boot({ app, translator });
+      const [api, appRef] = boot({ app, translator });
       appRef.current = {
-        addComponent: jest.fn(),
+        setComps: jest.fn(),
       };
 
-      await act(() => {
-        renderCallback();
-        api.add('foo');
-        return rendered;
-      });
+      mockedRoot.render.mock.calls[0][0].props.renderCallback();
+      await api.add('foo');
       expect(appRef.current.setComps).toHaveBeenCalledTimes(1);
     });
     test('should remove component', async () => {
       const app = { id: 'foo' };
       const translator = {};
-      const [api, appRef, rendered] = boot({ app, translator });
+      const [api, appRef] = boot({ app, translator });
       appRef.current = {
         setComps: jest.fn(),
       };
 
-      await act(() => {
-        renderCallback();
-        api.remove('foo');
-        return rendered;
-      });
+      mockedRoot.render.mock.calls[0][0].props.renderCallback();
+      await api.remove('foo');
       expect(appRef.current.setComps).toHaveBeenCalledTimes(1);
     });
     test('should set mui theme', async () => {
       const app = { id: 'foo' };
       const translator = {};
-      const [api, appRef, rendered] = boot({ app, translator });
+      const [api, appRef] = boot({ app, translator });
       appRef.current = {
         setMuiThemeName: jest.fn(),
       };
 
-      await act(() => {
-        renderCallback();
-        api.setMuiThemeName('wh0p');
-        return rendered;
-      });
+      mockedRoot.render.mock.calls[0][0].props.renderCallback();
+      await api.setMuiThemeName('wh0p');
+
       expect(appRef.current.setMuiThemeName).toHaveBeenCalledTimes(1);
     });
     test('should set context', async () => {
       const app = { id: 'foo' };
       const translator = {};
-      const [api, appRef, rendered] = boot({ app, translator });
+      const [api, appRef] = boot({ app, translator });
       appRef.current = {
         setContext: jest.fn(),
       };
 
-      await act(() => {
-        renderCallback();
-        api.context('ctx');
-        return rendered;
-      });
+      mockedRoot.render.mock.calls[0][0].props.renderCallback();
+      await api.context('ctx');
+
       expect(appRef.current.setContext).toHaveBeenCalledTimes(1);
     });
     test('should get app selections', async () => {
       const app = { id: 'foo' };
       const translator = {};
-      const [api, appRef, rendered] = boot({ app, translator });
+      const [api, appRef] = boot({ app, translator });
       appRef.current = {
         getAppSelections: jest.fn().mockReturnValue('app-selections'),
       };
 
-      await act(() => {
-        renderCallback();
-        api.getAppSelections();
-        return rendered;
-      });
+      mockedRoot.render.mock.calls[0][0].props.renderCallback();
+      await api.getAppSelections();
+
       expect(await appRef.current.getAppSelections()).toBe('app-selections');
     });
   });
