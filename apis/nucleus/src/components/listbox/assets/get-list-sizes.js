0bb055614c17b8d2c53c900c91df110ca27afa9d
Johan Lahti <johanlahti@gmail.com>
2023-03-15 15:58:23 +0100

    fix(#146): fall back to auto mode in col case

--- apis/nucleus/src/components/listbox/assets/get-list-sizes.js
+++ apis/nucleus/src/components/listbox/assets/get-list-sizes.js
@@ -36,10 +36,23 @@ function calculateRowMode({ maxVisibleColumns, listCount, containerWidth, column
 function calculateColumnMode({ maxVisibleRows, itemHeight, listCount, listHeight, columnAutoWidth, containerWidth }) {
   let rowCount;
   const maxRows = maxVisibleRows?.maxRows || 3;
+  const autoRowCount = Math.floor(listHeight / itemHeight);
   if (maxVisibleRows.auto !== false) {
-    rowCount = Math.floor(listHeight / itemHeight);
+    rowCount = autoRowCount;
   } else {
     rowCount = Math.min(listCount, maxRows);
+    const itemsCanFit = itemHeight * rowCount <= listHeight;
+    if (!itemsCanFit) {
+      // If items cannot fit inside height, fall back to auto mode.
+      return calculateColumnMode({
+        maxVisibleRows: { ...maxVisibleRows, auto: true },
+        itemHeight,
+        listCount,
+        listHeight,
+        columnAutoWidth,
+        containerWidth,
+      });
+    }
   }
 
   const columnCount = Math.ceil(listCount / rowCount);
