64a3b201dd9938a94765a4948230b3a8e7337624
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2023-03-24 16:30:59 +0100

    fix: simplyfy html structure for text highlight (#1187)
    * fix: simplyfy html structure for text highlight
    
    move text highlight to a separate level of spans inside the labelText
    remove the need check the text for direction (dir="auto" works)
    makes it easier to style and less affected by other styling
    
    * fix: hide correct end of text for text overflow

--- apis/nucleus/src/components/listbox/components/ListBoxRowColumn/components/LabelTag.jsx
+++ apis/nucleus/src/components/listbox/components/ListBoxRowColumn/components/LabelTag.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ValueField from './ValueField';
 
-function LabelTag({ label, color, highlighted, dense, showGray, checkboxes, cell, valueTextAlign }) {
+function LabelTag({ label, color, dense, showGray, checkboxes, cell, valueTextAlign }) {
   if (typeof label === 'string') {
     return (
       <ValueField
         label={label}
         color={color}
-        highlighted={highlighted}
         dense={dense}
         showGray={showGray}
         checkboxes={checkboxes}
