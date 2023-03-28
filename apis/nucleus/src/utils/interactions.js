61b82bed6e80a7a877bfe51299e08e3ce889dd3d
caele <tsm@qlik.com>
2023-03-28 17:10:15 +0200

    chore: conditionally assign things

--- apis/nucleus/src/utils/interactions.js
+++ apis/nucleus/src/utils/interactions.js
@@ -6,7 +6,7 @@ const DEFAULT_INTERACTIONS = {
 };
 
 function pickConstraintsOrInteractions(context) {
-  const { constraints, interactions } = context;
+  const { constraints = {}, interactions = {} } = context;
   // If some interaction is defined, then we use that
   if (Object.keys(interactions).length > 0) {
     return interactions;
@@ -19,15 +19,18 @@ function pickConstraintsOrInteractions(context) {
 }
 
 export default function unifyContraintsAndInteractions(context) {
-  const { constraints, interactions } = context;
-  const definedSettings = pickConstraintsOrInteractions(context);
-  Object.keys(DEFAULT_INTERACTIONS).forEach((state) => {
-    if (definedSettings[state] !== undefined) {
-      interactions[state] = definedSettings[state];
-      constraints[state] = !definedSettings[state];
-    } else {
-      interactions[state] = DEFAULT_INTERACTIONS[state];
-      constraints[state] = !DEFAULT_INTERACTIONS[state];
-    }
-  });
+  if (context) {
+    context.interactions = context.interactions || {};
+    context.constraints = context.constraints || {};
+    const definedSettings = pickConstraintsOrInteractions(context);
+    Object.keys(DEFAULT_INTERACTIONS).forEach((state) => {
+      if (definedSettings[state] !== undefined) {
+        context.interactions[state] = definedSettings[state];
+        context.constraints[state] = !definedSettings[state];
+      } else {
+        context.interactions[state] = DEFAULT_INTERACTIONS[state];
+        context.constraints[state] = !DEFAULT_INTERACTIONS[state];
+      }
+    });
+  }
 }
