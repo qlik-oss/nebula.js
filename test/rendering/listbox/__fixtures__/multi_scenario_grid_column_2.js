f4b83b6f8101ba3a10b40a0312fd3af16562f217
Per Nylander <per.nylander@qlik.com>
2023-03-28 11:15:42 +0200

    test: adding some more rendering tests for listbox (#1192)
    * test: adding some more rendering tests for list
    
    * test: changed fixture and include histogram
    
    * test: add grid rendering tests
    
    * test: addition to gitignore
    
    * test: add baselines

---
+++ test/rendering/listbox/__fixtures__/multi_scenario_grid_column_2.js
@@ -0,0 +1,432 @@
+const fixture = {
+  getLayout: () => ({
+    qInfo: {
+      qId: '07082dde-7f61-48cf-8b3a-e0a8775ee1e7',
+      qType: 'listbox',
+    },
+    qMeta: {
+      privileges: ['read', 'update', 'delete'],
+    },
+    qSelectionInfo: {},
+    qListObject: {
+      qSize: {
+        qcx: 1,
+        qcy: 17,
+      },
+      qDimensionInfo: {
+        qFallbackTitle: 'Basket Product Group Desc',
+        qApprMaxGlyphCount: 19,
+        qCardinal: 17,
+        qSortIndicator: 'A',
+        qGroupFallbackTitles: ['Basket Product Group Desc'],
+        qGroupPos: 0,
+        qStateCounts: {
+          qLocked: 0,
+          qSelected: 0,
+          qOption: 17,
+          qDeselected: 0,
+          qAlternative: 0,
+          qExcluded: 0,
+          qSelectedExcluded: 0,
+          qLockedExcluded: 0,
+        },
+        qTags: ['$ascii', '$text'],
+        qDimensionType: 'D',
+        qGrouping: 'N',
+        qNumFormat: {
+          qType: 'U',
+          qnDec: 0,
+          qUseThou: 0,
+        },
+        qIsAutoFormat: true,
+        qGroupFieldDefs: ['Basket Product Group Desc'],
+        qMin: 0,
+        qMax: 0,
+        qAttrExprInfo: [],
+        qAttrDimInfo: [],
+        qCardinalities: {
+          qCardinal: 17,
+          qHypercubeCardinal: 0,
+          qAllValuesCardinal: -1,
+        },
+        qLibraryId: 'XGvpzM',
+        title: 'Basket Product Group Desc',
+        coloring: {
+          changeHash: '0.45993295453224325',
+          hasValueColors: false,
+        },
+        autoSort: true,
+        cId: 'AHjFGWn',
+        textAlign: {
+          auto: false,
+          align: 'center',
+        },
+      },
+      qExpressions: [],
+      qDataPages: [],
+      showTitles: true,
+      title: '',
+      subtitle: '',
+      footnote: '',
+      disableNavMenu: false,
+      showDetails: true,
+      showDetailsExpression: false,
+      qOtherTotalSpec: {},
+      frequencyEnabled: true,
+    },
+    showTitles: true,
+    title: 'Basket Product Group Desc',
+    subtitle: '',
+    footnote: '',
+    disableNavMenu: false,
+    showDetails: true,
+    showDetailsExpression: false,
+    visualization: 'listbox',
+    layoutOptions: {
+      dataLayout: 'grid',
+      layoutOrder: 'row',
+      maxVisibleRows: {
+        auto: false,
+        maxRows: 10,
+      },
+      dense: false,
+      maxVisibleColumns: {
+        auto: false,
+      },
+    },
+    checkboxes: true,
+    frequencyMax: null,
+  }),
+  getListObjectData: () => [
+    {
+      qMatrix: [
+        [
+          {
+            qText: 'Alcoholic Beverages',
+            qNum: 'NaN',
+            qElemNumber: 11,
+            qState: 'O',
+            qFrequency: '52',
+          },
+        ],
+        [
+          {
+            qText: 'Baked Goods',
+            qNum: 'NaN',
+            qElemNumber: 13,
+            qState: 'O',
+            qFrequency: '52',
+          },
+        ],
+        [
+          {
+            qText: 'Baking Goods',
+            qNum: 'NaN',
+            qElemNumber: 8,
+            qState: 'O',
+            qFrequency: '146',
+          },
+        ],
+        [
+          {
+            qText: 'Beverages',
+            qNum: 'NaN',
+            qElemNumber: 7,
+            qState: 'O',
+            qFrequency: '134',
+          },
+        ],
+        [
+          {
+            qText: 'Breakfast Foods',
+            qNum: 'NaN',
+            qElemNumber: 10,
+            qState: 'O',
+            qFrequency: '34',
+          },
+        ],
+        [
+          {
+            qText: 'Canned Foods',
+            qNum: 'NaN',
+            qElemNumber: 2,
+            qState: 'O',
+            qFrequency: '186',
+          },
+        ],
+        [
+          {
+            qText: 'Canned Products',
+            qNum: 'NaN',
+            qElemNumber: 14,
+            qState: 'O',
+            qFrequency: '20',
+          },
+        ],
+        [
+          {
+            qText: 'Dairy',
+            qNum: 'NaN',
+            qElemNumber: 5,
+            qState: 'O',
+            qFrequency: '174',
+          },
+        ],
+        [
+          {
+            qText: 'Deli',
+            qNum: 'NaN',
+            qElemNumber: 3,
+            qState: 'O',
+            qFrequency: '120',
+          },
+        ],
+        [
+          {
+            qText: 'Eggs',
+            qNum: 'NaN',
+            qElemNumber: 9,
+            qState: 'O',
+            qFrequency: '34',
+          },
+        ],
+        [
+          {
+            qText: 'Frozen Foods',
+            qNum: 'NaN',
+            qElemNumber: 0,
+            qState: 'O',
+            qFrequency: '190',
+          },
+        ],
+        [
+          {
+            qText: 'Meat',
+            qNum: 'NaN',
+            qElemNumber: 15,
+            qState: 'O',
+            qFrequency: '10',
+          },
+        ],
+        [
+          {
+            qText: 'Produce',
+            qNum: 'NaN',
+            qElemNumber: 1,
+            qState: 'O',
+            qFrequency: '274',
+          },
+        ],
+        [
+          {
+            qText: 'Seafood',
+            qNum: 'NaN',
+            qElemNumber: 16,
+            qState: 'O',
+            qFrequency: '12',
+          },
+        ],
+        [
+          {
+            qText: 'Snack Foods',
+            qNum: 'NaN',
+            qElemNumber: 6,
+            qState: 'O',
+            qFrequency: '220',
+          },
+        ],
+        [
+          {
+            qText: 'Snacks',
+            qNum: 'NaN',
+            qElemNumber: 12,
+            qState: 'O',
+            qFrequency: '50',
+          },
+        ],
+        [
+          {
+            qText: 'Starchy Foods',
+            qNum: 'NaN',
+            qElemNumber: 4,
+            qState: 'O',
+            qFrequency: '46',
+          },
+        ],
+      ],
+      qTails: [],
+      qArea: {
+        qLeft: 0,
+        qTop: 0,
+        qWidth: 1,
+        qHeight: 17,
+      },
+    },
+    {
+      qMatrix: [
+        [
+          {
+            qText: 'Alcoholic Beverages',
+            qNum: 'NaN',
+            qElemNumber: 11,
+            qState: 'O',
+            qFrequency: '52',
+          },
+        ],
+        [
+          {
+            qText: 'Baked Goods',
+            qNum: 'NaN',
+            qElemNumber: 13,
+            qState: 'O',
+            qFrequency: '52',
+          },
+        ],
+        [
+          {
+            qText: 'Baking Goods',
+            qNum: 'NaN',
+            qElemNumber: 8,
+            qState: 'O',
+            qFrequency: '146',
+          },
+        ],
+        [
+          {
+            qText: 'Beverages',
+            qNum: 'NaN',
+            qElemNumber: 7,
+            qState: 'O',
+            qFrequency: '134',
+          },
+        ],
+        [
+          {
+            qText: 'Breakfast Foods',
+            qNum: 'NaN',
+            qElemNumber: 10,
+            qState: 'O',
+            qFrequency: '34',
+          },
+        ],
+        [
+          {
+            qText: 'Canned Foods',
+            qNum: 'NaN',
+            qElemNumber: 2,
+            qState: 'O',
+            qFrequency: '186',
+          },
+        ],
+        [
+          {
+            qText: 'Canned Products',
+            qNum: 'NaN',
+            qElemNumber: 14,
+            qState: 'O',
+            qFrequency: '20',
+          },
+        ],
+        [
+          {
+            qText: 'Dairy',
+            qNum: 'NaN',
+            qElemNumber: 5,
+            qState: 'O',
+            qFrequency: '174',
+          },
+        ],
+        [
+          {
+            qText: 'Deli',
+            qNum: 'NaN',
+            qElemNumber: 3,
+            qState: 'O',
+            qFrequency: '120',
+          },
+        ],
+        [
+          {
+            qText: 'Eggs',
+            qNum: 'NaN',
+            qElemNumber: 9,
+            qState: 'O',
+            qFrequency: '34',
+          },
+        ],
+        [
+          {
+            qText: 'Frozen Foods',
+            qNum: 'NaN',
+            qElemNumber: 0,
+            qState: 'O',
+            qFrequency: '190',
+          },
+        ],
+        [
+          {
+            qText: 'Meat',
+            qNum: 'NaN',
+            qElemNumber: 15,
+            qState: 'O',
+            qFrequency: '10',
+          },
+        ],
+        [
+          {
+            qText: 'Produce',
+            qNum: 'NaN',
+            qElemNumber: 1,
+            qState: 'O',
+            qFrequency: '274',
+          },
+        ],
+        [
+          {
+            qText: 'Seafood',
+            qNum: 'NaN',
+            qElemNumber: 16,
+            qState: 'O',
+            qFrequency: '12',
+          },
+        ],
+        [
+          {
+            qText: 'Snack Foods',
+            qNum: 'NaN',
+            qElemNumber: 6,
+            qState: 'O',
+            qFrequency: '220',
+          },
+        ],
+        [
+          {
+            qText: 'Snacks',
+            qNum: 'NaN',
+            qElemNumber: 12,
+            qState: 'O',
+            qFrequency: '50',
+          },
+        ],
+        [
+          {
+            qText: 'Starchy Foods',
+            qNum: 'NaN',
+            qElemNumber: 4,
+            qState: 'O',
+            qFrequency: '46',
+          },
+        ],
+      ],
+      qTails: [],
+      qArea: {
+        qLeft: 0,
+        qTop: 0,
+        qWidth: 1,
+        qHeight: 17,
+      },
+    },
+  ],
+};
+
+export default fixture;
