5f6148cdeeb527881cd8942b575271ede7e81775
Quan Ho <quan.ho@qlik.com>
2023-03-30 08:54:11 +0200

    refactor: avoid defining a component inside another component (#1199)

--- apis/nucleus/src/components/listbox/components/ListBoxError.jsx
+++ apis/nucleus/src/components/listbox/components/ListBoxError.jsx
@@ -2,15 +2,15 @@ import { styled } from '@mui/material/styles';
 import React, { useContext } from 'react';
 import InstanceContext from '../../../contexts/InstanceContext';
 
+const StyledDiv = styled('div')(() => ({
+  display: 'flex',
+  width: '100%',
+  height: '100%',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
 export default function ListBoxError({ text }) {
   const { translator } = useContext(InstanceContext);
-  const StyledDiv = styled('div')(() => ({
-    display: 'flex',
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-
   return <StyledDiv>{translator.get(text)}</StyledDiv>;
 }
