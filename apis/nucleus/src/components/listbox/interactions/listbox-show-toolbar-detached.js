33eaa57cf4392a0807ecb426955e3ead3bd57a87
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2023-04-12 10:52:39 +0200

    fix: refactor to work with Grid flex styling (#1216)

--- apis/nucleus/src/components/listbox/interactions/listbox-show-toolbar-detached.js
+++ apis/nucleus/src/components/listbox/interactions/listbox-show-toolbar-detached.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-nested-ternary */
 export default function showToolbarDetached({ containerRef, titleRef, iconsWidth }) {
   const containerWidth = containerRef?.current?.clientWidth ?? 0;
   const padding = 16;
@@ -7,7 +6,6 @@ export default function showToolbarDetached({ containerRef, titleRef, iconsWidth
   const notSufficientSpace = containerWidth < contentWidth + actionToolbarWidth;
   const isTruncated = titleRef?.current?.scrollWidth > titleRef?.current?.offsetWidth;
   const isDetached = !!(notSufficientSpace | isTruncated);
-  const reasonDetached = isDetached ? (notSufficientSpace ? 'noSpace' : 'truncated') : '';
 
-  return { isDetached, reasonDetached };
+  return isDetached;
 }
