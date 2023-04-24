4df9595fb2c3654acd55437a0bbcf8d8a2fe4ae3
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2023-04-24 12:34:21 +0200

    fix(listbox): detach toolbar on resize (#1232)
    * fix: detach toolbar on resize
    
    * fix: add logical or

--- apis/nucleus/src/components/listbox/interactions/__tests__/listbox-show-toolbar-detached.test.js
+++ apis/nucleus/src/components/listbox/interactions/__tests__/listbox-show-toolbar-detached.test.js
@@ -4,20 +4,20 @@ const iconsWidth = 28;
 
 describe('show listbox toolbar detached', () => {
   it('should return true if there is not enough space for toolbar', () => {
-    const containerRef = { current: { clientWidth: 100 } };
+    const containerRect = { width: 100 };
     const titleRef = { current: { clientWidth: 60, scrollWidth: 80, offsetWidth: 81 } };
-    expect(showToolbarDetached({ containerRef, titleRef, iconsWidth })).toStrictEqual(true);
+    expect(showToolbarDetached({ containerRect, titleRef, iconsWidth })).toStrictEqual(true);
   });
 
   it('should return true if title is truncated', () => {
-    const containerRef = { current: { clientWidth: 300 } };
+    const containerRect = { width: 300 };
     const titleRef = { current: { clientWidth: 60, scrollWidth: 200, offsetWidth: 199 } };
-    expect(showToolbarDetached({ containerRef, titleRef, iconsWidth })).toStrictEqual(true);
+    expect(showToolbarDetached({ containerRect, titleRef, iconsWidth })).toStrictEqual(true);
   });
 
   it('should return false if there is enough space for title and toolbar', () => {
-    const containerRef = { current: { clientWidth: 300 } };
+    const containerRect = { width: 300 };
     const titleRef = { current: { clientWidth: 60, scrollWidth: 200, offsetWidth: 201 } };
-    expect(showToolbarDetached({ containerRef, titleRef, iconsWidth })).toStrictEqual(false);
+    expect(showToolbarDetached({ containerRect, titleRef, iconsWidth })).toStrictEqual(false);
   });
 });
