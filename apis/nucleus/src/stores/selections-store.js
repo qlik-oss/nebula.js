f966352e49590a67d6a9b91b1264b75012c4ce02
quanho <quan.ho@qlik.com>
2023-05-03 16:15:14 +0200

    fix: fix selection close when there are many objects with the same model

--- apis/nucleus/src/stores/selections-store.js
+++ apis/nucleus/src/stores/selections-store.js
@@ -2,16 +2,13 @@ import createKeyStore from './create-key-store';
 
 const [useAppSelectionsStore, appSelectionsStore] = createKeyStore({});
 const [useAppModalStore, appModalStore] = createKeyStore({});
-const [useObjectSelectionsStore, objectSelectionsStore] = createKeyStore({});
 const [useModalObjectStore, modalObjectStore] = createKeyStore({});
 
 export {
   useAppSelectionsStore,
   useAppModalStore,
-  useObjectSelectionsStore,
   appSelectionsStore,
   appModalStore,
-  objectSelectionsStore,
   useModalObjectStore,
   modalObjectStore,
 };
