cfaee8aea800c5b6c3a7cd4d96c11113add09d6e
caele <tsm@qlik.com>
2023-05-22 11:22:25 +0200

    chore: set update snapshots to all

--- test/rendering/playwright.config.rendering.js
+++ test/rendering/playwright.config.rendering.js
@@ -12,6 +12,7 @@ const config = {
     timeout: 30000,
   },
   workers: process.env.CI ? 1 : undefined,
+  updateSnapshots: 'all',
   use: {
     browserName: 'chromium',
     actionTimeout: 30000,
