8bce244028917ca715105d2833ab3466bc4c66d0
Daniel Sjöstrand <99665802+DanielS-Qlik@users.noreply.github.com>
2023-05-25 14:51:05 +0200

    fix: prevent truncation

--- apis/nucleus/src/components/listbox/interactions/listbox-show-toolbar-detached.js
+++ apis/nucleus/src/components/listbox/interactions/listbox-show-toolbar-detached.js
@@ -6,9 +6,9 @@ export default function showToolbarDetached({
   headerPaddingRight,
 }) {
   const containerWidth = containerRect.width;
-  // const hasIcon = iconsWidth > 0;
-  const padding = headerPaddingLeft + headerPaddingRight; // HEADER_PADDING_RIGHT + (hasIcon ? CELL_PADDING_LEFT : 2);
-  const contentWidth = (titleRef?.current?.clientWidth ?? 0) + iconsWidth + padding;
+  const preventTruncation = 2;
+  const padding = headerPaddingLeft + headerPaddingRight;
+  const contentWidth = (titleRef?.current?.clientWidth ?? 0) + iconsWidth + padding + preventTruncation;
   const actionToolbarWidth = 128;
   const notSufficientSpace = containerWidth < contentWidth + actionToolbarWidth;
   const isTruncated = titleRef?.current?.scrollWidth > titleRef?.current?.offsetWidth;
