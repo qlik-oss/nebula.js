05ee185da197d1f1b78e97f4ba709543854d0ee4
Christian Veinfors <cvs@qlik.com>
2023-05-25 16:19:14 +0200

    test: fix unit test and update images

--- apis/nucleus/src/components/listbox/interactions/__tests__/listbox-show-toolbar-detached.test.js
+++ apis/nucleus/src/components/listbox/interactions/__tests__/listbox-show-toolbar-detached.test.js
@@ -1,23 +1,31 @@
 import showToolbarDetached from '../listbox-show-toolbar-detached';
 
 const iconsWidth = 28;
+const headerPaddingLeft = 9;
+const headerPaddingRight = 4;
 
 describe('show listbox toolbar detached', () => {
   it('should return true if there is not enough space for toolbar', () => {
     const containerRect = { width: 100 };
-    const titleRef = { current: { clientWidth: 60, scrollWidth: 80, offsetWidth: 81 } };
-    expect(showToolbarDetached({ containerRect, titleRef, iconsWidth })).toStrictEqual(true);
+    const titleRef = { current: { clientWidth: 50, scrollWidth: 80, offsetWidth: 81 } };
+    expect(
+      showToolbarDetached({ containerRect, titleRef, iconsWidth, headerPaddingLeft, headerPaddingRight })
+    ).toStrictEqual(true);
   });
 
   it('should return true if title is truncated', () => {
     const containerRect = { width: 300 };
-    const titleRef = { current: { clientWidth: 60, scrollWidth: 200, offsetWidth: 199 } };
+    const titleRef = {
+      current: { clientWidth: 60, scrollWidth: 200, offsetWidth: 199, headerPaddingLeft, headerPaddingRight },
+    };
     expect(showToolbarDetached({ containerRect, titleRef, iconsWidth })).toStrictEqual(true);
   });
 
   it('should return false if there is enough space for title and toolbar', () => {
     const containerRect = { width: 300 };
-    const titleRef = { current: { clientWidth: 60, scrollWidth: 200, offsetWidth: 201 } };
+    const titleRef = {
+      current: { clientWidth: 60, scrollWidth: 200, offsetWidth: 201, headerPaddingLeft, headerPaddingRight },
+    };
     expect(showToolbarDetached({ containerRect, titleRef, iconsWidth })).toStrictEqual(false);
   });
 });
