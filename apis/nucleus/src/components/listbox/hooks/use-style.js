5af6e7e30149830aadaf3fbf8eb3990e21a0952b
costa <costantino.marullo@qlik.com>
2023-09-01 08:33:37 +0200

    fix: getvalue utils

--- apis/nucleus/src/components/listbox/hooks/use-style.js
+++ apis/nucleus/src/components/listbox/hooks/use-style.js
@@ -1,27 +1,7 @@
-function getValue(object, path, fallback) {
-  if (object === undefined || path === undefined) {
-    return fallback;
-  }
-
-  const steps = path.split('.');
-
-  let scoped = object;
-
-  for (let i = 0; i < steps.length; ++i) {
-    const step = steps[i];
-
-    if (scoped[step] === undefined) {
-      return fallback;
-    }
-
-    scoped = scoped[step];
-  }
-
-  return scoped;
-}
+import utils from '@nebula.js/conversion/src/utils';
 
 export default function createStyleService({ theme, layout }) {
-  const getLayoutValue = (reference, fallback) => getValue(layout, reference, fallback);
+  const getLayoutValue = (reference, fallback) => utils.getValue(layout, reference, fallback);
 
   const overrides = (key) => getLayoutValue('components', []).find((c) => c.key === key) || undefined;
 
