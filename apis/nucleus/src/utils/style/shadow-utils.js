f139781a273b60416e9d7a4289fd22c11d9af9fb
Christian Veinfors <cvs@qlik.com>
2023-11-21 09:52:24 +0100

    docs: remove internal fn from api spec

--- apis/nucleus/src/utils/style/shadow-utils.js
+++ apis/nucleus/src/utils/style/shadow-utils.js
@@ -19,6 +19,7 @@ export const getShadows = (shadowString) => {
 
 /**
  * Combine box shadow with box shadow color.
+ * @ignore
  * @param {string} boxShadow - Box shadow which may include color
  * @param {string} themeBoxShadow - Box shadow from theme which may include color
  * @param {string | undefined} boxShadowColor - Box shadow color
