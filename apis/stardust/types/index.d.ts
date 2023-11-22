25ef2d439e8ef445d76905d2c48f45d2414c95f6
Christian Veinfors <cvs@qlik.com>
2023-11-22 11:49:39 +0100

    docs: remove internal fn from api spec (#1410)
    * docs: remove internal fn from api spec
    
    * docs: change from ignore to private tag

--- apis/stardust/types/index.d.ts
+++ apis/stardust/types/index.d.ts
@@ -570,8 +570,6 @@ declare namespace stardust {
         meta?: object;
     }
 
-    type getFullBoxShadow = any;
-
     interface ActionToolbarElement extends HTMLElement{
         className: "njs-action-toolbar-popover";
     }
