a280282f8816cd039c631c57bd609714e8154500
linhnguyen-qlik <lgu@qlik.com>
2024-03-05 11:00:09 +0100

    test: add unit test

---
+++ apis/nucleus/src/components/listbox/components/__tests__/utils.test.js
@@ -0,0 +1,42 @@
+import DrillDownIcon from '@nebula.js/ui/icons/drill-down';
+import CyclicIcon from '@nebula.js/ui/icons/reload';
+import utils from '../ListBoxHeader/utils';
+
+describe('utils', () => {
+  beforeEach(() => {});
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should return no icon data for single dimension', () => {
+    const dimInfo = { qGrouping: 'N' };
+    const result = utils.createDimensionIconData(dimInfo, undefined);
+    expect(result).toEqual(undefined);
+  });
+
+  it('should return icon data for drilldown dimension', () => {
+    const dimInfo = { qGrouping: 'H' };
+    const result = utils.createDimensionIconData(dimInfo, undefined);
+    expect(result).toMatchObject({
+      icon: DrillDownIcon,
+      tooltip: 'Tooltip.dimensions.drilldown',
+      onClick: undefined,
+    });
+  });
+
+  it('should return no icon data for cyclic dimension', () => {
+    const dimInfo = { qGrouping: 'C' };
+    const app = {};
+    const result = utils.createDimensionIconData(dimInfo, app);
+    expect(result).toMatchObject({
+      icon: CyclicIcon,
+      tooltip: 'Tooltip.dimensions.cyclic',
+      onClick: expect.any(Function),
+    });
+  });
+});
