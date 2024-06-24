49a01eac6ae36dc8a8d603834fb567210603c84b
Max Gefvert <max.gefvert@qlik.com>
2024-06-24 13:23:26 +0200

    fix: lint

--- apis/nucleus/src/__tests__/index.test.js
+++ apis/nucleus/src/__tests__/index.test.js
@@ -67,7 +67,7 @@ describe('nuked()', () => {
               fieldIdentifier,
               options,
               flags: {
-                isEnabled: expect.any(Function)
+                isEnabled: expect.any(Function),
               },
               key: expect.any(String),
               stateName: '$',
