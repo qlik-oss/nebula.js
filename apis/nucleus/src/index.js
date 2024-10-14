6dcaede36cda4ab8db0fe7f40852df9ae56b8e05
caele <tsm@qlik.com>
2024-10-14 08:51:39 +0200

    feat: add host config to config and galaxy

--- apis/nucleus/src/index.js
+++ apis/nucleus/src/index.js
@@ -88,6 +88,7 @@ const DEFAULT_SNAPSHOT_CONFIG = /** @lends SnapshotConfiguration */ {
  * @property {Context=} context Settings for the rendering instance
  * @property {Array<TypeInfo>=} types Visualization types to register
  * @property {Array<ThemeInfo>=} themes Themes to register
+ * @property {object=} hostConfig a qlik api compatible host config, see https://github.com/qlik-oss/qlik-api-ts/blob/main/docs/authentication.md#the-host-config
  * @property {object=} anything
  * @example
  * import { embed } from '@nebula.js/stardust'
@@ -242,6 +243,8 @@ function nuked(configuration = {}) {
         flags: flagsFn(configuration.flags),
         /** @type {string} */
         deviceType: deviceTypeFn(configuration.context.deviceType),
+        /** @type {object} */
+        hostConfig: configuration.hostConfig,
         /** @type {object} */
         anything: configuration.anything,
       },
