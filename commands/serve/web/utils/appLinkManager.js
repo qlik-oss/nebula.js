be59c4cf75b866448c26f6966810fb76806cbb12
Tobias Åström <tsm@qlik.com>
2025-02-07 15:04:33 +0100

    fix: prefix from app list (#1675)
    * fix: prefix from app list
    
    * chore: add https option

--- commands/serve/web/utils/appLinkManager.js
+++ commands/serve/web/utils/appLinkManager.js
@@ -1,7 +1,9 @@
 export const getAppLink = ({ navigate, location, targetApp, info }) => {
   const { search } = location;
   const protocol = info.enigma.secure ? 'wss' : 'ws';
-  const host = info.enigma.host === 'localhost' ? `${info.enigma.host}:${info.enigma.port}` : info.enigma.host;
+  const port = info.enigma.port ? `:${info.enigma.port}` : '';
+  const prefix = info.enigma.prefix ? `/${info.enigma.prefix}` : '';
+  const host = (info.enigma.host === 'localhost' ? `${info.enigma.host}${port}` : info.enigma.host) + prefix;
   const newEngineUrl = `${protocol}://${host}/app/${encodeURIComponent(targetApp)}`;
   const modifiedEngineUrl = search.replace(info.engineUrl, newEngineUrl);
 
