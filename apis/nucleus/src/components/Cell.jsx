e518c706afc4fc71e18e8b0d303ee41d3c9a5af6
linhnguyen-qlik <lgu@qlik.com>
2025-04-07 14:11:41 +0200

    fix: check blurCallback properly

--- apis/nucleus/src/components/Cell.jsx
+++ apis/nucleus/src/components/Cell.jsx
@@ -377,7 +377,7 @@ const Cell = forwardRef(
 
     focusHandler.current.blurCallback = (resetFocus) => {
       halo.root.toggleFocusOfCells();
-      if (resetFocus && typeof blurCallback === 'function') {
+      if (resetFocus && typeof customFocusHandler?.blurCallback === 'function') {
         customFocusHandler.blurCallback(resetFocus);
         return;
       }
