d37a1ff045f955be57102f421e6eb366ab5114a8
caele <tsm@qlik.com>
2025-05-05 08:28:59 +0200

    fix: work with client mixins

--- apis/nucleus/src/hooks/useLayout.js
+++ apis/nucleus/src/hooks/useLayout.js
@@ -1,7 +1,11 @@
 import useRpc from './useRpc';
 
 export default function useLayout(model) {
-  return useRpc(model, 'getLayout');
+  const [layout, { validating, canCancel, canRetry }, longrunning] = useRpc(model, 'getLayout');
+  if (model?.pureLayout) {
+    return [model.pureLayout, { validating, canCancel, canRetry }, longrunning];
+  }
+  return [layout, { validating, canCancel, canRetry }, longrunning];
 }
 
 export function useAppLayout(model) {
