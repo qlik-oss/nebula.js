d6e11d562bccc5f7dc40374e8d9a436b758ecf18
Jingjing <8jjxie@gmail.com>
2025-05-20 10:27:53 +0200

    fix: conflict

--- apis/stardust/types/index.d.ts
+++ apis/stardust/types/index.d.ts
@@ -478,6 +478,14 @@ declare namespace stardust {
 
     }
 
+    interface Flags {
+        /**
+         * Checks whether the specified flag is enabled.
+         * @param flag The value flag to check.
+         */
+        isEnabled(flag: string): boolean;
+    }
+
     class AppSelections {
         constructor();
 
@@ -550,14 +558,6 @@ declare namespace stardust {
          */
         noModal(accept?: boolean): Promise<undefined>;
 
-    }
-
-    interface Flags {
-        /**
-         * Checks whether the specified flag is enabled.
-         * @param flag The value flag to check.
-         */
-        isEnabled(flag: string): boolean;
     }
 
     type Field = string | qix.NxDimension | qix.NxMeasure | stardust.LibraryField;
