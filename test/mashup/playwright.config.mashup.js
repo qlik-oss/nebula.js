8a6c2817a004a8fd240312ec8cdbeb69136773dc
Tobias Åström <tsm@qlik.com>
2025-06-19 16:21:09 +0200

    chore: convert mashup tests (#1770)
    * chore: start migrate mashup tetss
    
    * chore: rename file
    
    * chore: add log
    
    * chore: mashup test config
    
    * chore: higher timeout
    
    * chore: more config
    
    * chore: reporter
    
    * chore: change config
    
    * chore: change config
    
    * chore: change config
    
    * chore: change config
    
    * chore: store artifacts
    
    * chore: longer timeout
    
    * chore: migrate listbox test
    
    * chore: update waitForFunc

---
+++ test/mashup/playwright.config.mashup.js
@@ -0,0 +1,27 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  reporter: [
+    ['dot'],
+    ['html', { outputFolder: './reports/html' }],
+    ['junit', { outputFile: './test/mashup/reports/xml/report.xml' }],
+  ],
+  testDir: './',
+  globalSetup: './setup.cjs',
+  forbidOnly: !!process.env.CI,
+  timeout: 60000,
+  expect: {
+    toMatchSnapshot: { threshold: 0.00025 },
+    timeout: 30000,
+  },
+  workers: 1,
+  use: {
+    browserName: 'chromium',
+    actionTimeout: 30000,
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+});
