1295c9809de962c2cc859392bbc902f50d55ac68
caele <tsm@qlik.com>
2025-06-19 13:50:42 +0200

    chore: more config

--- test/mashup/playwright.config.mashup.cjs
+++ test/mashup/playwright.config.mashup.cjs
@@ -2,4 +2,12 @@ module.exports = {
   // ...other config...
   globalSetup: require.resolve('./setup.cjs'),
   timeout: 10000,
+  workers: 1,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
 };
