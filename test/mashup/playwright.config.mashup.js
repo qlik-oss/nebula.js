78e63307496cbcef4f0cb71d93aa82ed27706065
caele <tsm@qlik.com>
2025-06-19 14:08:37 +0200

    chore: change config

--- test/mashup/playwright.config.mashup.js
+++ test/mashup/playwright.config.mashup.js
@@ -1,18 +1,27 @@
-module.exports = {
-  // ...other config...
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
   reporter: [
     ['dot'],
     ['html', { outputFolder: './reports/html' }],
     ['junit', { outputFile: './test/rendering/reports/xml/report.xml' }],
   ],
+  testDir: './',
   globalSetup: require.resolve('./setup.cjs'),
-  timeout: 10000,
+  forbidOnly: !!process.env.CI,
+  timeout: 60000,
+  expect: {
+    toMatchSnapshot: { threshold: 0.00025 },
+    timeout: 30000,
+  },
   workers: 1,
   use: {
+    browserName: 'chromium',
+    actionTimeout: 30000,
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
   },
-};
+});
