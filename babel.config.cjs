a66bd68ddcf7f1a681588cb857e49f072e436993
caele <tsm@qlik.com>
2025-10-01 22:28:26 +0200

    chore: add back to root

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,10 @@
+const { run } = require('jest');
+
 module.exports = {
   env: {
     test: {
       presets: [['@babel/preset-env', { targets: { node: 'current' } }, '@babel/preset-react']],
+      plugins: ['@babel/plugin-transform-react-jsx'],
     },
   },
 };
