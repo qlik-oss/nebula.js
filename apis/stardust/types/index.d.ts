d8e87f0904a4dc130e4580e3500cc3602c86c53b
Jingjing <8jjxie@gmail.com>
2025-12-11 14:08:22 +0100

    fix: run yarn spec

--- apis/stardust/types/index.d.ts
+++ apis/stardust/types/index.d.ts
@@ -633,6 +633,14 @@ declare namespace stardust {
 
     }
 
+    /**
+     * Sort items shown in current selections toolbar.
+     * The order is as follows:
+     * 	1. qOneAndOnlyOne
+     * 	2. qSortIndex
+     */
+    type sortSelections = (a: Item | Object, b: Item | Object)=>void;
+
     class Navigation implements stardust.Emitter {
         constructor();
 
