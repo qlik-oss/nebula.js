47d207467093e41b20dd0bca9e6b3251a1fb7de7
Elise <fdq@qlik.com>
2025-12-15 13:30:10 +0100

    fix: add waterfall chart to mapping since it is documented (#1871)

--- apis/nucleus/src/hooks/useStyling.js
+++ apis/nucleus/src/hooks/useStyling.js
@@ -28,6 +28,7 @@ const THEME_OBJECT_TYPE_MAP = {
   mapchart: 'mapChart',
   bulletchart: 'bulletChart',
   'sn-table': 'straightTableV2',
+  waterfallchart: 'waterfallChart',
 };
 
 const getThemeObjectType = (visualization) => {
