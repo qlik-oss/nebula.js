caab3e4421f2f7aa14c253d26be1bf2dc81d6390
caele <tsm@qlik.com>
2026-08-03 16:56:47 +0200

    chore: import creation test

--- .github/scripts/nebula_create.sh
+++ .github/scripts/nebula_create.sh
@@ -10,14 +10,16 @@ TEST="${6:-true}"
 
 if [ "$MASHUP" = "true" ]; then
   echo "Create mashup project"
-  ./commands/cli/lib/index.js create mashup "$PROJECT_NAME" --install "$INSTALL" --pkgm yarn
+  ./commands/cli/lib/index.js create mashup "$PROJECT_NAME" --install false --pkgm yarn
 else
   echo "Create project based on Picasso template"
-  ./commands/cli/lib/index.js create "$PROJECT_NAME" --picasso "$PICASSO_TEMPLATE" --install "$INSTALL" --pkgm yarn
+  ./commands/cli/lib/index.js create "$PROJECT_NAME" --picasso "$PICASSO_TEMPLATE" --install false --pkgm yarn
 fi
 touch "$PROJECT_NAME"/yarn.lock
-echo "Install generated project dependencies"
-YARN_ENABLE_HARDENED_MODE=0 yarn --cwd "$PROJECT_NAME"
+if [ "$INSTALL" = "true" ]; then
+  echo "Install generated project dependencies"
+  YARN_ENABLE_HARDENED_MODE=0 YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn --cwd "$PROJECT_NAME" install
+fi
 echo "Linking packages"
 cd "$PROJECT_NAME"
 yarn link ../../apis/stardust
@@ -27,7 +29,11 @@ yarn link ../../commands/build
 echo "Log node_modules/@nebula.js"
 ls -la node_modules/@nebula.js
 if [ "$BUILD" = "true" ]; then
-  yarn run build
+  if [ "$MASHUP" = "true" ]; then
+    NODE_OPTIONS=--preserve-symlinks yarn run build
+  else
+    yarn run build
+  fi
 fi
 if [ "$TEST" = "true" ]; then
   echo "Installing Playwright browser for this generated project"
